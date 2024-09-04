bd0baf26f16b04926944f32674e39b6424bc83a3
alexeh <alexurola@gmail.com>
2024-09-04 07:27:18 +0200

    Initial TF project setup with remote state

---
+++ infrastructure/modules/state/variables.tf
@@ -0,0 +1,16 @@
+variable "state_aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = "eu-west-3"
+}
+
+variable "state_project_name" {
+  type        = string
+  description = "Short name of the project, will be used to prefix created resources"
+}
+
+variable "state_aws_profile" {
+    type        = string
+    description = "AWS profile to use to perform TF operations"
+}
+
---
+++ infrastructure/variables.tf
@@ -0,0 +1,30 @@
+variable "aws_profile" {
+  type        = string
+  description = "AWS profile to use to perform TF operations"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = "eu-west-3"
+}
+
+variable "allowed_account_id" {
+  type        = string
+  description = "AWS account id"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Short name of the project, will be used to prefix created resources"
+}
+
+variable "github_owner" {
+  type        = string
+  description = "Owner of the Github repository where the code is hosted"
+}
+
+variable "github_token" {
+  type        = string
+  description = "Github token to access the repository"
+}
