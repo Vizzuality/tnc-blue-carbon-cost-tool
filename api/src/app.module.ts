b2c661a9533950604a17807f139b4188f98c538c
alexeh <alexurola@gmail.com>
2024-09-05 09:33:48 +0200

    docker compose setup

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'node:process';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      envFilePath: [
+        join(__dirname, `../../shared/config/.env.${process.env.NODE_ENV}`),
+        join(__dirname, '../../shared/config/.env'),
+      ],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      // TODO: Move this to config service method
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+        synchronize: true,
+        ssl:
+          process.env.NODE_ENV === 'production'
+            ? { require: true, rejectUnauthorized: false }
+            : false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
