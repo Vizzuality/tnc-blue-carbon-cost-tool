4b1a3a49f5a7fc60d796d61ee5f02c58e45b2eb2
alexeh <alexurola@gmail.com>
2024-09-05 07:34:10 +0200

    basic placeholders

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -14,13 +14,12 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Placeholder for{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
+              Blue Carbon Cost Tool
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
