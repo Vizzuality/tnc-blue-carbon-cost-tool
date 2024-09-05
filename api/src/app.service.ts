d85f050a4ce04fe268c30594efeeafa0dbb5be06
alexeh <alexurola@gmail.com>
2024-09-05 09:50:36 +0200

    add api tests and deploy workflows

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import * as process from 'node:process';
 
 @Injectable()
 export class AppService {
-  constructor(private config: ConfigService) {}
   getHello(): string {
-    console.log(this.config.get('JWT_SECRET'));
-    console.log('process', process.env.JWT_SECRET);
     return 'Hello Blue Coast Carbon Tool!';
   }
 }
