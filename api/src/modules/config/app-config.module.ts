b2f4e2a3024d5786e87dfbb5230bddc1880a3133
alexeh <alexurola@gmail.com>
2024-09-07 07:36:28 +0200

    refactor api configuration

---
+++ api/src/modules/config/app-config.module.ts
@@ -0,0 +1,29 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { ApiConfigService } from '@api/modules/config/app-config.service';
+import { DatabaseModule } from '@api/modules/config/database/database.module';
+
+@Global()
+@Module({
+  imports: [
+    DatabaseModule,
+    /**
+     * @note: Check if we can abstract the conf to ApiConfigService
+     */
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      envFilePath: [
+        join(
+          __dirname,
+          `../../../../shared/config/.env.${process.env.NODE_ENV}`,
+        ),
+        join(__dirname, '../../../../shared/config/.env'),
+      ],
+    }),
+  ],
+  providers: [ConfigService, ApiConfigService],
+  exports: [ApiConfigService, DatabaseModule],
+})
+export class ApiConfigModule {}
