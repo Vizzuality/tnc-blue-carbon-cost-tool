507157ddad4f1b6d1d23d51ee9599728fce348c9
alexeh <alexurola@gmail.com>
2024-09-14 07:47:08 +0200

    add aws ses env vars

--- infrastructure/modules/env/api_env_vars.tf
+++ infrastructure/modules/env/api_env_vars.tf
@@ -5,6 +5,9 @@ resource "random_password" "jwt_secret" {
   override_special = "!#%&*()-_=+[]{}<>:?"
 }
 
+resource "aws_iam_access_key" "email_user_access_key" {
+  user = module.email.iam_user.name
+}
 
 locals {
   api_secret_env_vars = {
@@ -14,6 +17,9 @@ locals {
     DB_USERNAME = module.postgresql.username
     DB_PORT = module.postgresql.port
     JWT_SECRET = random_password.jwt_secret.result
+    AWS_SES_ACCESS_KEY_ID = aws_iam_access_key.email_user_access_key.id
+    AWS_SES_ACCESS_KEY_SECRET = aws_iam_access_key.email_user_access_key.secret
+    AWS_SES_DOMAIN = module.email.mail_from_domain
   }
   api_env_vars = {
 
