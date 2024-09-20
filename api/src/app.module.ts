518314416c314925fd908e3269e8b116c78c07e5
alexeh <alexurola@gmail.com>
2024-09-20 07:34:36 +0200

    remove global jwt and role guards to be handled more atomically

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,20 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiConfigModule } from '@api/modules/config/app-config.module';
-import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from '@api/modules/auth/auth.module';
-import { JwtAuthGuard } from '@api/modules/auth/guards/jwt-auth.guard';
-import { RolesGuard } from '@api/modules/auth/guards/roles.guard';
 import { NotificationsModule } from '@api/modules/notifications/notifications.module';
 import { EventsModule } from '@api/modules/events/events.module';
 
 @Module({
   imports: [ApiConfigModule, AuthModule, NotificationsModule, EventsModule],
   controllers: [AppController],
-  providers: [
-    AppService,
-    { provide: APP_GUARD, useClass: JwtAuthGuard },
-    { provide: APP_GUARD, useClass: RolesGuard },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
