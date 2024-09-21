ddbe2f3d93acfbdd3a911e2c792b7dde341a8f2a
alexeh <alexurola@gmail.com>
2024-09-21 14:54:44 +0200

    Remove role hierarchy

--- api/src/modules/admin/admin.controller.ts
+++ api/src/modules/admin/admin.controller.ts
@@ -1,4 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '@api/modules/auth/guards/roles.guard';
 
 @Controller('admin')
+@UseGuards(AuthGuard)
+@UseGuards(RolesGuard)
 export class AdminController {}
