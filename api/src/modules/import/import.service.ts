38414770d8dd2287db2362f5349389666100d9ca
alexeh <alexurola@gmail.com>
2024-09-29 11:07:08 +0200

    WIP - inserting data

--- api/src/modules/import/import.service.ts
+++ api/src/modules/import/import.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { XlsxParser } from '@api/modules/import/services/xlsx.parser';
+import { EcoSystemDataRepository } from '@api/modules/data/ecosystem-data.repository';
+import { EcosystemProject } from '@api/modules/data/ecosystem-data.entity';
 
 @Injectable()
-export class ImportService {}
+export class ImportService {
+  constructor(
+    private readonly xlsxParser: XlsxParser,
+    private readonly repo: EcoSystemDataRepository,
+  ) {}
+
+  async import(file: Express.Multer.File) {
+    let data;
+    try {
+      data = data = await this.xlsxParser.parseExcel<EcosystemProject>(
+        file.buffer,
+      );
+      await this.repo.insertData(data);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      return data;
+    }
+  }
+}
--- api/src/modules/import/services/xlsx.parser.ts
+++ api/src/modules/import/services/xlsx.parser.ts
@@ -1,13 +1,32 @@
-import { ExcelParserInterface } from '@api/modules/import/services/excel-parser.interface';
 import { Injectable } from '@nestjs/common';
 import { read, utils, WorkBook, WorkSheet } from 'xlsx';
+import { ExcelParserInterface } from './excel-parser.interface';
 
 @Injectable()
 export class XlsxParser implements ExcelParserInterface {
-  async parseExcel(buffer: Buffer): Promise<any> {
+  async parseExcel<T>(buffer: Buffer): Promise<T[]> {
     const workbook: WorkBook = read(buffer);
     const sheet: WorkSheet = workbook.Sheets['master_table'];
-    const data: any[] = utils.sheet_to_json(sheet, { raw: true, defval: null });
-    return data;
+    const data: T[] = utils.sheet_to_json(sheet, {
+      raw: true,
+      defval: null,
+    });
+    return data.map((row) => this.handleCrap<T>(row));
+  }
+
+  // TODO: temporal hack to handle stuff, there are values that are No data that could be null in the excel, and missing values like country code or continent
+  //       double check the entity to update it
+
+  private handleCrap<T>(row: T): T {
+    return Object.fromEntries(
+      Object.entries(row).map(([key, value]) => [
+        key,
+        value === 'No data'
+          ? null
+          : typeof value === 'string' && !isNaN(Number(value))
+            ? Number(value)
+            : value,
+      ]),
+    ) as T;
   }
 }
