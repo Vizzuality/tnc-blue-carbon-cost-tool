5cc0cdc46317981d636fef706192994f48d932ac
alexeh <alexurola@gmail.com>
2024-10-01 10:26:06 +0200

    adds country entity

---
+++ shared/entities/countries/country.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+export enum CONTINENTS {
+  AFRICA = "Africa",
+  ASIA = "Asia",
+  EUROPE = "Europe",
+  NORTH_AMERICA = "North America",
+  OCEANIA = "Oceania",
+  SOUTH_AMERICA = "South America",
+}
+
+@Entity("countries")
+export class Country {
+  @PrimaryColumn({ length: 3 })
+  code: string;
+
+  @Column({ length: 100 })
+  country: string;
+
+  @Column({ type: "enum", enum: CONTINENTS })
+  continent: CONTINENTS;
+
+  @Column({ name: "region_1", length: 50, nullable: true })
+  region1: string;
+
+  @Column({ name: "region_2", length: 50, nullable: true })
+  region2: string;
+
+  @Column({ name: "numeric_code", length: 3, nullable: true })
+  numericCode: string;
+
+  @Column({ name: "hdi", type: "int" })
+  hdi: number;
+}
