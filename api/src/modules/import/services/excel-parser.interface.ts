e60b4555fd6ee5eadf890170f189945a7eeeb39a
alexeh <alexurola@gmail.com>
2024-10-02 07:18:54 +0200

    Initial excel to db map

--- api/src/modules/import/services/excel-parser.interface.ts
+++ api/src/modules/import/services/excel-parser.interface.ts
@@ -1,15 +1,5 @@
 export const ExcelParserToken = Symbol('ExcelParserInterface');
 
-export const EXCEL_TO_DB_MAP = {
-  'Country Code': 'code',
-  'Country Name': 'country',
-  Continent: 'continent',
-  'Region 1': 'region1',
-  'Region 2': 'region2',
-  'Numeric Code': 'numericCode',
-  HDI: 'hdi',
-};
-
 export interface ExcelParserInterface {
   parseExcel(data: Buffer): Promise<any>;
 }
