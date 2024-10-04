bb085f29ff534f19c5e62dc92bf4e590ad423da9
alexeh <alexurola@gmail.com>
2024-10-04 11:06:06 +0200

    update project_size based on master data

--- api/src/modules/base-data/project-size.entity.ts
+++ api/src/modules/base-data/project-size.entity.ts
@@ -13,7 +13,7 @@ export class ProjectSize {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ length: 3 })
+  @Column({ length: 3, unique: true })
   country_code: string;
 
   // Unidirectional relation
@@ -21,21 +21,21 @@ export class ProjectSize {
   @JoinColumn({ name: 'country_code' })
   country: Country;
 
-  @Column('numeric')
+  @Column('numeric', { nullable: true })
   mangrove_restored_area: number;
 
-  @Column('numeric')
+  @Column('numeric', { nullable: true })
   seagrass_restored_area: number;
 
-  @Column('numeric')
+  @Column('numeric', { nullable: true })
   salt_marsh_restored_area: number;
 
-  @Column('numeric')
+  @Column('numeric', { nullable: true })
   mangrove_conserved_area: number;
 
-  @Column('numeric')
+  @Column('numeric', { nullable: true })
   seagrass_conserved_area: number;
 
-  @Column('numeric')
+  @Column('numeric', { nullable: true })
   salt_marsh_conserved_area: number;
 }
