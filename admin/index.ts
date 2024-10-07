b9f9d66af7015cd1c8ac829f245f21acff3a1421
alexeh <alexurola@gmail.com>
2024-10-07 07:26:27 +0200

    Add minimal typeorm conf. running

--- admin/index.ts
+++ admin/index.ts
@@ -1,13 +1,26 @@
-import AdminJS from "adminjs";
+import "reflect-metadata";
+import AdminJS, { ComponentLoader } from "adminjs";
 import AdminJSExpress from "@adminjs/express";
 import express from "express";
+import * as AdminJSTypeorm from "@adminjs/typeorm";
+
+AdminJS.registerAdapter({
+  Database: AdminJSTypeorm.Database,
+  Resource: AdminJSTypeorm.Resource,
+});
 
 const PORT = 3000;
+
+const componentLoader = new ComponentLoader();
 
 const start = async () => {
   const app = express();
 
-  const admin = new AdminJS({});
+  const admin = new AdminJS({
+    rootPath: "/administration",
+    componentLoader,
+    // resources: [{ resource: User }],
+  });
 
   const adminRouter = AdminJSExpress.buildRouter(admin);
   app.use(admin.options.rootPath, adminRouter);
