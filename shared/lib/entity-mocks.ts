5f9e9e548f8d98ca2cd2e6c7e5f7f55b231e6dbc
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2024-10-08 11:34:31 +0200

    implements e2e auth

---
+++ shared/lib/entity-mocks.ts
@@ -0,0 +1,21 @@
+import { genSalt, hash } from 'bcrypt';
+import { DataSource, DeepPartial } from 'typeorm';
+import { User } from "@shared/entities/users/user.entity";
+
+
+export const createUser = async (
+  dataSource: DataSource,
+  additionalData?: Partial<User>,
+): Promise<User> => {
+  const salt = await genSalt();
+  const usedPassword = additionalData?.password ?? '12345678';
+  const defaultData: DeepPartial<User> = {
+    email: 'test@user.com',
+    password: await hash(usedPassword, salt),
+  };
+
+  const user = { ...defaultData, ...additionalData };
+
+  await dataSource.getRepository(User).save(user);
+  return { ...user, password: usedPassword } as User;
+};
