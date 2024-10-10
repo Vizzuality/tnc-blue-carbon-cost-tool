d6824ef553bba4955ebd0d45ce7edb613a6480b9
alexeh <alexurola@gmail.com>
2024-10-10 09:20:46 +0200

    refactor createUser mock

--- shared/lib/entity-mocks.ts
+++ shared/lib/entity-mocks.ts
@@ -1,20 +1,19 @@
-import { genSalt, hash } from 'bcrypt';
-import { DataSource, DeepPartial } from 'typeorm';
+import { genSalt, hash } from "bcrypt";
+import { DataSource, DeepPartial } from "typeorm";
 import { User } from "@shared/entities/users/user.entity";
-
 
 export const createUser = async (
   dataSource: DataSource,
   additionalData?: Partial<User>,
 ): Promise<User> => {
   const salt = await genSalt();
-  const usedPassword = additionalData?.password ?? '12345678';
-  const defaultData: DeepPartial<User> = {
-    email: 'test@user.com',
+  const usedPassword = additionalData?.password ?? "12345678";
+  const user = {
+    email: "test@user.com",
+    ...additionalData,
     password: await hash(usedPassword, salt),
+    isActive: additionalData?.isActive ?? true,
   };
-
-  const user = { ...defaultData, ...additionalData };
 
   await dataSource.getRepository(User).save(user);
   return { ...user, password: usedPassword } as User;
