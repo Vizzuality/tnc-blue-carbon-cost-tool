85d1e21b96510362f2e1789c1d6b8f4e09f8108b
alexeh <alexurola@gmail.com>
2024-10-10 11:53:54 +0200

    fix e2e tests

--- e2e/tests/auth/auth.spec.ts
+++ e2e/tests/auth/auth.spec.ts
@@ -5,22 +5,37 @@ import { User } from "@shared/entities/users/user.entity";
 let testManager: E2eTestManager;
 let page: Page;
 
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage();
-  testManager = await E2eTestManager.load(page);
-});
+test.describe.configure({ mode: "serial" });
 
-test.beforeEach(async () => {
-  await testManager.clearDatabase();
-});
+test.describe("Auth", () => {
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    testManager = await E2eTestManager.load(page);
+  });
 
-test.afterEach(async () => {
-  //await testManager.clearDatabase();
-});
+  test.beforeEach(async () => {
+    await testManager.clearDatabase();
+  });
 
-test.afterAll(async () => {
-  await testManager.logout();
-  await testManager.close();
+  test.afterEach(async () => {
+    // await testManager.clearDatabase();
+  });
+
+  test.afterAll(async () => {
+    //await testManager.logout();
+    await testManager.close();
+  });
+
+  test("an user signs in successfully", async () => {
+    const user: Pick<User, "email" | "password" | "partnerName"> = {
+      email: "jhondoe@test.com",
+      password: "12345678",
+      partnerName: "admin",
+    };
+    await testManager.mocks().createUser(user);
+    await testManager.login(user as User);
+    await expect(page.getByText(`Email: ${user.email}`)).toBeVisible();
+  });
 });
 
 // test("an user signs up successfully", async ({ page }) => {
@@ -48,43 +63,4 @@ test.afterAll(async () => {
 //   await expect(await page.locator('input[type="email"]')).toHaveValue(
 //     user.email,
 //   );
-// });
-
-test("an user signs in successfully", async ({ page }) => {
-  const user: Pick<User, "email" | "password" | "partnerName"> = {
-    email: "jhondoe@test.com",
-    password: "12345678",
-    partnerName: "admin",
-  };
-
-  await testManager.mocks().createUser(user);
-  await page.goto("/auth/signin");
-  await page.getByLabel("Email").fill(user.email);
-  await page.locator('input[type="password"]').fill(user.password);
-  await page.getByRole("button", { name: /log in/i }).click();
-});
-
-test("test", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
-  await page.getByRole("link", { name: "Sign in" }).click();
-  await page.getByPlaceholder("Enter your email").click();
-  await page.getByPlaceholder("Enter your email").fill("jhondoe@test.com");
-  await page.getByPlaceholder("*******").click();
-  await page.getByPlaceholder("*******").fill("12345678");
-  await page.getByRole("button", { name: "Log in" }).click();
-});
-// test("test", async ({ page }) => {
-//   const user: Pick<User, "email" | "password" | "partnerName"> = {
-//     email: "jhondoe@test.com",
-//     password: "12345678",
-//     partnerName: "admin",
-//   };
-//   await page.goto("http://localhost:3000/");
-//   await page.getByRole("link", { name: "Sign in" }).click();
-//   await page.getByPlaceholder("Enter your email").click();
-//   await page.getByPlaceholder("Enter your email").fill(user.email);
-//   await page.getByPlaceholder("*******").click();
-//   await page.getByPlaceholder("*******").fill(user.password);
-//   await page.getByRole("button", { name: "Log in" }).click();
-//   await expect(page.getByText(`Email: ${user.email}`)).toBeVisible();
 // });
