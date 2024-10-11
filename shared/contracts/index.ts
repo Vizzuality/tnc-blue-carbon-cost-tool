6f32ce9397c520eb339e54079dc51a2908aa9911
alexeh <alexurola@gmail.com>
2024-10-11 06:38:04 +0200

    Add router base conf

--- shared/contracts/index.ts
+++ shared/contracts/index.ts
@@ -2,11 +2,29 @@ import { initContract } from "@ts-rest/core";
 import { adminContract } from "@shared/contracts/admin.contract";
 import { authContract } from "@shared/contracts/auth.contract";
 import { usersContract } from "@shared/contracts/users.contract";
+import { JSONAPIError } from "@shared/dtos/json-api.error";
 
 const contract = initContract();
 
-export const router = contract.router({
-  auth: authContract,
-  admin: adminContract,
-  user: usersContract,
-});
+export const router = contract.router(
+  {
+    auth: authContract,
+    admin: adminContract,
+    user: usersContract,
+  },
+  {
+    commonResponses: {
+      400: contract.type<JSONAPIError>(),
+      401: contract.type<JSONAPIError>(),
+      403: contract.type<JSONAPIError>(),
+      404: contract.type<JSONAPIError>(),
+      500: contract.type<JSONAPIError>(),
+    },
+    strictStatusCodes: true,
+  },
+);
+
+const baseRouterConfig = contract.router(
+  { authContract, adminContract, usersContract },
+  {},
+);
