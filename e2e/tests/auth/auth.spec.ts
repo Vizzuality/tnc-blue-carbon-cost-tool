0a3849b9316a7211240cc2a63453e3573fe12d0a
alexeh <alexurola@gmail.com>
2024-10-12 10:06:58 +0200

    update e2e sign up test to use new token

--- e2e/tests/auth/auth.spec.ts
+++ e2e/tests/auth/auth.spec.ts
@@ -1,6 +1,7 @@
-import { test, expect, Page } from "@playwright/test";
+import { expect, Page, test } from "@playwright/test";
 import { E2eTestManager } from "@shared/lib/e2e-test-manager";
 import { User } from "@shared/entities/users/user.entity";
+import { TOKEN_TYPE_ENUM } from "@shared/schemas/auth/token-type.schema";
 
 let testManager: E2eTestManager;
 let page: Page;
@@ -46,30 +47,45 @@ test.describe("Auth", () => {
     const newPassword = "987654321987654321";
 
     const userCreated = await testManager.mocks().createUser(user);
-    const userToken = await testManager.generateToken(userCreated)
+    const userToken = await testManager.generateTokenByType(
+      userCreated,
+      TOKEN_TYPE_ENUM.SIGN_UP,
+    );
 
     await page.goto(`/auth/signup/${userToken}`);
 
-    await page.getByPlaceholder('Enter the One-Time Password received in your mail').click();
-    await page.getByPlaceholder('Enter the One-Time Password received in your mail').fill(user.password);
-    await page.getByPlaceholder('Create a password').click();
-    await page.getByPlaceholder('Create a password').fill(newPassword);
-    await page.getByPlaceholder('Repeat the password').click();
-    await page.getByPlaceholder('Repeat the password').fill(newPassword);
+    await page
+      .getByPlaceholder("Enter the One-Time Password received in your mail")
+      .click();
+    await page
+      .getByPlaceholder("Enter the One-Time Password received in your mail")
+      .fill(user.password);
+    await page.getByPlaceholder("Create a password").click();
+    await page.getByPlaceholder("Create a password").fill(newPassword);
+    await page.getByPlaceholder("Repeat the password").click();
+    await page.getByPlaceholder("Repeat the password").fill(newPassword);
 
     await page.getByRole("button", { name: /sign up/i }).click();
 
-    await page.waitForURL("/auth/signin")
-    await expect(page.getByRole('heading', { name: 'Welcome to Blue Carbon Cost' })).toBeVisible();
+    await page.waitForURL("/auth/signin");
+    await expect(
+      page.getByRole("heading", { name: "Welcome to Blue Carbon Cost" }),
+    ).toBeVisible();
   });
 
   test("an user signs up with an invalid token", async ({ page }) => {
     await page.goto("/auth/signup/12345678");
 
-    await expect(page.getByText('The token is invalid or has expired.')).toBeVisible();
-    await expect(page.getByPlaceholder('Enter the One-Time Password received in your mail')).toBeDisabled();
-    await expect(page.getByPlaceholder('Create a password')).toBeDisabled();
-    await expect(page.getByPlaceholder('Repeat the password')).toBeDisabled();
+    await expect(
+      page.getByText("The token is invalid or has expired."),
+    ).toBeVisible();
+    await expect(
+      page.getByPlaceholder(
+        "Enter the One-Time Password received in your mail",
+      ),
+    ).toBeDisabled();
+    await expect(page.getByPlaceholder("Create a password")).toBeDisabled();
+    await expect(page.getByPlaceholder("Repeat the password")).toBeDisabled();
     await expect(page.getByRole("button", { name: /sign up/i })).toBeDisabled();
   });
 });
