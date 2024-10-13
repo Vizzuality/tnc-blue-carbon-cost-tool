3aaf570106af36937a66385c3b94717fc646c65f
alexeh <alexurola@gmail.com>
2024-10-13 07:24:27 +0200

    use origin for mailing related operations

--- api/src/modules/auth/commands/request-password-recovery.command.ts
+++ api/src/modules/auth/commands/request-password-recovery.command.ts
@@ -1,3 +1,6 @@
 export class RequestPasswordRecoveryCommand {
-  constructor(public readonly email: string) {}
+  constructor(
+    public readonly email: string,
+    public readonly origin: string,
+  ) {}
 }
