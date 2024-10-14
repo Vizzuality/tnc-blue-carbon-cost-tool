483fd30604018ce2ef7356a0a0cc2eaf7cbf01fb
alexeh <alexurola@gmail.com>
2024-10-14 12:30:31 +0200

    add missing env vars

--- infrastructure/modules/env/api_env_vars.tf
+++ infrastructure/modules/env/api_env_vars.tf
@@ -32,6 +32,8 @@ locals {
     RESET_PASSWORD_TOKEN_EXPIRES_IN = "24h"
     EMAIL_CONFIRMATION_TOKEN_SECRET = random_password.email_confirmation_token_secret.result
     EMAIL_CONFIRMATION_TOKEN_EXPIRES_IN = "24h"
+    SIGN_UP_TOKEN_SECRET = random_password.email_confirmation_token_secret.result
+    SIGN_UP_TOKEN_EXPIRES_IN = "24h"
     AWS_SES_ACCESS_KEY_ID = aws_iam_access_key.email_user_access_key.id
     AWS_SES_ACCESS_KEY_SECRET = aws_iam_access_key.email_user_access_key.secret
     AWS_SES_DOMAIN = module.email.mail_from_domain
