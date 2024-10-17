74cb0126c926196e63f14c2ed76772f85ea73176
alexeh <alexurola@gmail.com>
2024-10-17 08:34:43 +0200

    add role to admin add user schema

--- shared/schemas/users/create-user.schema.ts
+++ shared/schemas/users/create-user.schema.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
+import { ROLES } from "@shared/entities/users/roles.enum";
 
 export const CreateUserSchema = z.object({
   email: z.string().email(),
   name: z.string().optional(),
   partnerName: z.string(),
+  role: z.enum([ROLES.ADMIN, ROLES.PARTNER]),
 });
