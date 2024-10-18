b608dfa6fec9486e9d7587069fd733060d7af63b
alexeh <alexurola@gmail.com>
2024-10-18 05:40:04 +0200

    add OTP to welcome email, flow working

--- api/src/modules/notifications/email/templates/welcome-email.template.ts
+++ api/src/modules/notifications/email/templates/welcome-email.template.ts
@@ -1,12 +1,17 @@
-export const WELCOME_EMAIL_HTML_CONTENT = (url, expiration): string => `
+export const WELCOME_EMAIL_HTML_CONTENT = (
+  url: string,
+  expiration: string,
+  oneTimePassword: string,
+): string => `
     <h1>Dear User,</h1>
     <br/>
-    <p>We recently received a request to reset your password for your account. If you made this request, please click on the link below to securely change your password:</p>
+    <p>Welcome to the TNC Blue Carbon Cost Tool Platform</p>
     <br/>
-    <p><a href="${url}" target="_blank" rel="noopener noreferrer">Secure Password Reset Link</a></p>
+    <p>Thank you for signing up. We're excited to have you on board. Please active you account by signing up adding a password of your choice</p>
+    <p><a href="${url}" target="_blank" rel="noopener noreferrer">Sign Up Link</a></p>
     <br/>
-    <p>This link will direct you to our app to create a new password. For security reasons, this link will expire after ${expiration}.</p>
-    <p>If you did not request a password reset, please ignore this email; your password will remain the same.</p>
+    <p>Your one-time password is ${oneTimePassword}</p>
+    <p>For security reasons, this link will expire after ${expiration}.</p>
     <br/>
     <p>Thank you for using the platform. We're committed to ensuring your account's security.</p>
     <p>Best regards.</p>`;
