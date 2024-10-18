2edb8c5eefb298cc070c0b0296d74a4218dc12f0
alexeh <alexurola@gmail.com>
2024-10-18 06:28:55 +0200

    refactor event handling

--- api/src/modules/admin/admin.module.ts
+++ api/src/modules/admin/admin.module.ts
@@ -2,12 +2,9 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AuthModule } from '@api/modules/auth/auth.module';
 import { ImportModule } from '@api/modules/import/import.module';
-import { NewUserEventHandler } from '@api/modules/admin/events/handlers/new-user-event.handler';
-import { PasswordRecoveryRequestedEventHandler } from '@api/modules/admin/events/handlers/password-recovery-requested.handler';
 
 @Module({
   imports: [AuthModule, ImportModule],
-  providers: [NewUserEventHandler, PasswordRecoveryRequestedEventHandler],
   controllers: [AdminController],
 })
 export class AdminModule {}
