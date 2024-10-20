f432f79e5037df1f99e0e4eff49d4eed7b4f315c
alexeh <alexurola@gmail.com>
2024-10-20 06:31:17 +0200

    add map controller and repo scaffold

--- api/src/modules/countries/map/map.controller.ts
+++ api/src/modules/countries/map/map.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { MapRepository } from '@api/modules/countries/map/map.repository';
+import { Country } from '@shared/entities/countries/country.entity';
+import { GeoJSON } from 'geojson';
 
 @Controller('map')
 export class MapController {
+  constructor(private readonly mapRepository: MapRepository) {}
   @Get('/geojson')
-  getGeoJson() {
-    return 'This will return the geojson of the countries';
+  getGeoJson(
+    @Query('countryCode') countryCode: Country['countryCode'],
+  ): Promise<GeoJSON> {
+    return this.mapRepository.getGeoJson(countryCode);
   }
 }
--- api/src/modules/countries/map/map.repository.ts
+++ api/src/modules/countries/map/map.repository.ts
@@ -2,6 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Country } from '@shared/entities/countries/country.entity';
+import { GeoJSON } from 'geojson';
+
+/**
+ * @description: The aim for this repository is to work with geospatial data, for now "geometry" column in countries
+ * table. The country repository will be used to work with the metadata of the countries and avoid loading geometries when only metadata is needed,
+ * which can consume a lot of resources.
+ */
 
 @Injectable()
 export class MapRepository extends Repository<Country> {
@@ -10,5 +17,15 @@ export class MapRepository extends Repository<Country> {
     private readonly repository: Repository<Country>,
   ) {
     super(repository.target, repository.manager, repository.queryRunner);
+  }
+
+  async getGeoJson(countryCode: Country['countryCode']): Promise<GeoJSON> {
+    const res = await this.createQueryBuilder('country')
+      .select('ST_AsGeoJSON(country.geometry)', 'geojson')
+      //.where('country.country_code = :countryCode', { countryCode: 'ESP' })
+      .getRawMany();
+
+    console.log(res);
+    return res as unknown as GeoJSON;
   }
 }
