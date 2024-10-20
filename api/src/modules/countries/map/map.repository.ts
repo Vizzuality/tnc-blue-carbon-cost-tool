652eef849cc620b0fa72864a0fcc6c312fe97ec3
alexeh <alexurola@gmail.com>
2024-10-20 07:25:46 +0200

    get ST_Simplify version of Features to avoid memory crash

--- api/src/modules/countries/map/map.repository.ts
+++ api/src/modules/countries/map/map.repository.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { FeatureCollection, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Country } from '@shared/entities/countries/country.entity';
-import { GeoJSON } from 'geojson';
+import { BaseData } from '@api/modules/model/base-data.entity';
 
 /**
  * @description: The aim for this repository is to work with geospatial data, for now "geometry" column in countries
@@ -12,6 +12,7 @@ import { GeoJSON } from 'geojson';
 
 @Injectable()
 export class MapRepository extends Repository<Country> {
+  logger: Logger = new Logger(MapRepository.name);
   constructor(
     @InjectRepository(Country)
     private readonly repository: Repository<Country>,
@@ -19,13 +20,47 @@ export class MapRepository extends Repository<Country> {
     super(repository.target, repository.manager, repository.queryRunner);
   }
 
-  async getGeoJson(countryCode: Country['countryCode']): Promise<GeoJSON> {
-    const res = await this.createQueryBuilder('country')
-      .select('ST_AsGeoJSON(country.geometry)', 'geojson')
-      //.where('country.country_code = :countryCode', { countryCode: 'ESP' })
-      .getRawMany();
+  async getGeoFeatures(
+    countryCode: Country['countryCode'],
+  ): Promise<FeatureCollection> {
+    const queryBuilder = this.createQueryBuilder('country');
+    queryBuilder.innerJoin(
+      BaseData,
+      'bd',
+      'bd.country_code = country.country_code',
+    );
+    queryBuilder.select(
+      `
+        json_build_object(
+          'type', 'FeatureCollection',
+          'features', json_agg(
+            json_build_object(
+              'type', 'Feature',
+              'geometry', ST_AsGeoJSON(ST_Simplify(country.geometry, 0.01))::jsonb, 
+              'properties', json_build_object(${this.getPropertiesQuery()})
+            )
+          )
+        )`,
+      'geojson',
+    );
+    if (countryCode) {
+      queryBuilder.where('country.country_code = :countryCode', {
+        countryCode,
+      });
+    }
 
-    console.log(res);
-    return res as unknown as GeoJSON;
+    const result: FeatureCollection | undefined =
+      await queryBuilder.getRawOne<FeatureCollection>();
+    this.logger.log(`Retrieved geo features`);
+    if (!result) {
+      throw new NotFoundException(`Could not retrieve geo features`);
+    }
+    return result;
+  }
+
+  private getPropertiesQuery(): string {
+    return `'projectSize', bd.project_size_ha,
+            'feasibilityAnalysis', bd.feasibility_analysis,
+            'conservationPlanningAndAdmin', bd.conservation_planning_and_admin`;
   }
 }
