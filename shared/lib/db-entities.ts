63641f59cbc697547a3320740a84281a214a8f36
alexeh <alexurola@gmail.com>
2024-10-21 06:41:23 +0200

    add project size entity

--- shared/lib/db-entities.ts
+++ shared/lib/db-entities.ts
@@ -2,14 +2,12 @@ import { User } from "@shared/entities/users/user.entity";
 import { ApiEventsEntity } from "@api/modules/api-events/api-events.entity";
 import { Country } from "@api/modules/model/entities/country.entity";
 import { BaseData } from "@api/modules/model/base-data.entity";
-import { CostInput } from "@api/modules/model/entities/cost-input.entity";
-import { CarbonInputEntity } from "@api/modules/model/entities/carbon-input.entity";
+import { ProjectSize } from "@api/modules/model/entities/project-size.entity";
 
 export const DB_ENTITIES = [
   User,
   ApiEventsEntity,
   Country,
   BaseData,
-  CostInput,
-  CarbonInputEntity,
+  ProjectSize,
 ];
