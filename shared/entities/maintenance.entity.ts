bb485538e2edc4ecbe31c920f47729c86dd6d6b1
Catalin Oancea <catalin.oancea2008@gmail.com>
2024-10-23 11:54:51 +0300

    Add Maintenance table

---
+++ shared/entities/maintenance.entity.ts
@@ -0,0 +1,23 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  OneToOne,
+} from "typeorm";
+import { BaseData } from "@shared/entities/base-data.entity";
+
+@Entity("maintenance")
+export class Maintenance extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column("decimal", { name: "maintenance_cost_pc_of_impl_labor_cost" })
+  maintenanceCost: number;
+
+  @Column("decimal", { name: "maintenance_duration_years" })
+  maintenance_duration: number;
+
+  @OneToOne(() => BaseData, (baseData) => baseData.projectSize)
+  baseData: BaseData;
+}
