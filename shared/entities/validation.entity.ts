9687491d4eaef28c29522f6000ccea03e9975ea2
Catalin Oancea <catalin.oancea2008@gmail.com>
2024-10-23 11:34:53 +0300

    Add Validation Cost table

---
+++ shared/entities/validation.entity.ts
@@ -0,0 +1,20 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  OneToOne,
+} from "typeorm";
+import { BaseData } from "@shared/entities/base-data.entity";
+
+@Entity("validation_cost")
+export class ValidationCost extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column("decimal", { name: "validation_cost" })
+  validationCost: number;
+
+  @OneToOne(() => BaseData, (baseData) => baseData.projectSize)
+  baseData: BaseData;
+}
