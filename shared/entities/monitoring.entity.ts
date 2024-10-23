216aa0ea27ff589e72390f8686cdb81d99838328
Catalin Oancea <catalin.oancea2008@gmail.com>
2024-10-23 11:41:46 +0300

    Add Monitoring Cost table

---
+++ shared/entities/monitoring.entity.ts
@@ -0,0 +1,20 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  OneToOne,
+} from "typeorm";
+import { BaseData } from "@shared/entities/base-data.entity";
+
+@Entity("monitoring_cost")
+export class MonitoringCost extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column("decimal", { name: "monitoring_cost_per_year" })
+  monitoringCost: number;
+
+  @OneToOne(() => BaseData, (baseData) => baseData.projectSize)
+  baseData: BaseData;
+}
