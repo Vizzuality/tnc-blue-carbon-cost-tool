e44676352858806c368d5511249fc16e232ac53a
Catalin Oancea <catalin.oancea2008@gmail.com>
2024-10-23 12:02:19 +0300

    Add Community Benefit Sharing table

---
+++ shared/entities/community-benefit-sharing-fund.entity.ts
@@ -0,0 +1,20 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  OneToOne,
+} from "typeorm";
+import { BaseData } from "@shared/entities/base-data.entity";
+
+@Entity("community_benefit_sharing_fund")
+export class CommunityBenefitSharingFund extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column("decimal", { name: "benefit_sharing_fund_pc_of_revenue" })
+  benefitSharingFund: number;
+
+  @OneToOne(() => BaseData, (baseData) => baseData.communityRepresentation)
+  baseData: BaseData[];
+}
