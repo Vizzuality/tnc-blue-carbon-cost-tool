9b9e9d952736508ae5931d6160d6dfec69a94007
Catalin Oancea <catalin.oancea2008@gmail.com>
2024-10-23 11:39:13 +0300

    Add Implementation Labor Cost table

---
+++ shared/entities/implementation-labor.entity.ts
@@ -0,0 +1,20 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  OneToOne,
+} from "typeorm";
+import { BaseData } from "@shared/entities/base-data.entity";
+
+@Entity("implementation_labor_cost")
+export class ImplementationLaborCost extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column("decimal", { name: "implementation_labor_cost_per_ha" })
+  implementationLaborCost: number;
+
+  @OneToOne(() => BaseData, (baseData) => baseData.projectSize)
+  baseData: BaseData;
+}
