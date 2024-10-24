4ae3dd2a1ae48fe3dbc73e0b68b6a90fb6c5832c
alexeh <alexurola@gmail.com>
2024-10-24 06:38:31 +0200

    temporarily import countries and geoms at init

--- api/src/modules/import/import.service.ts
+++ api/src/modules/import/import.service.ts
@@ -1,24 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { XlsxParser } from '@api/modules/import/services/xlsx.parser';
 import { EntityPreprocessor } from '@api/modules/import/services/entity.preprocessor';
 import { BaseDataRepository } from '@api/modules/model/base-data.repository';
+import {
+  ExcelParserInterface,
+  ExcelParserToken,
+} from '@api/modules/import/services/excel-parser.interface';
+import { DataSource } from 'typeorm';
+import { Country } from '@shared/entities/country.entity';
 
 @Injectable()
 export class ImportService {
+  logger: Logger = new Logger(ImportService.name);
   constructor(
+    //@Inject(ExcelParserToken)
     private readonly xlsxParser: XlsxParser,
     private readonly repo: BaseDataRepository,
+    private readonly dataSource: DataSource,
     private readonly preprocessor: EntityPreprocessor,
   ) {}
 
   async import(fileBuffer: Buffer) {
     let data;
+    this.logger.log('Excel file import started...');
     try {
-      data = await this.xlsxParser.parseExcel<any>(fileBuffer);
+      data = await this.xlsxParser.parseExcel(fileBuffer);
       const dbEntities = this.preprocessor.toDbEntities(data);
+      const savedCountries = await this.dataSource
+        .getRepository(Country)
+        .save(dbEntities.countries);
 
       const dbResult = [];
-      return dbResult;
+      return savedCountries;
     } catch (e) {
       console.log(e);
     } finally {
