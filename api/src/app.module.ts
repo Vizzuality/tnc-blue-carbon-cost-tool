f213d0e87d38f279d5749763dc658158c1369f16
alexeh <alexurola@gmail.com>
2024-10-24 06:38:31 +0200

    temporarily import countries and geoms at init

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiConfigModule } from '@api/modules/config/app-config.module';
@@ -11,6 +11,9 @@ import { UsersModule } from '@api/modules/users/users.module';
 import { APP_FILTER } from '@nestjs/core';
 import { AllExceptionsFilter } from '@api/filters/all-exceptions.exception.filter';
 import { TsRestModule } from '@ts-rest/nest';
+import { DataSource } from 'typeorm';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Module({
   imports: [
@@ -34,4 +37,30 @@ import { TsRestModule } from '@ts-rest/nest';
     { provide: APP_FILTER, useClass: AllExceptionsFilter },
   ],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private datasource: DataSource) {}
+
+  async onModuleInit() {
+    const queryRunner = this.datasource.createQueryRunner();
+    try {
+      await queryRunner.connect();
+      const countries = await queryRunner.query(
+        'SELECT count(*) FROM countries',
+      );
+      if (countries[0].count > 0) {
+        return;
+      }
+      const sql = fs.readFileSync(
+        path.join(__dirname, '../../../src/geocountries.sql'),
+        'utf8',
+      );
+
+      await queryRunner.query(sql);
+      console.warn('Countries imported');
+    } catch (e) {
+      console.error('Error while importing countries', e);
+    } finally {
+      await queryRunner.release();
+    }
+  }
+}
--- api/src/modules/import/services/entity.preprocessor.ts
+++ api/src/modules/import/services/entity.preprocessor.ts
@@ -5,67 +5,76 @@ import { Country } from '@shared/entities/country.entity';
 
 export type ParsedDBEntities = {
   countries: Country[];
-  baseData: BaseData[];
+  //baseData: BaseData[];
 };
 
 @Injectable()
 export class EntityPreprocessor {
   toDbEntities(data: {
     Countries: any[];
-    master_table: any[];
+    //master_table: any[];
   }): ParsedDBEntities {
-    //   const { Countries: rawCountries, master_table: rawBaseData } = data;
-    //
-    //   const countries = this.processCountries(rawCountries);
-    //   const baseData = this.processBaseData(rawBaseData);
-    //
-    //   return {
-    //     countries,
-    //     baseData,
-    //   };
-    // }
-    //
-    // private processCountries(rawCountries: any[]): Country[] {
-    //   return rawCountries.map((rawCountry) => {
-    //     const country = new Country();
-    //     country.country = rawCountry.country;
-    //     country.countryCode = rawCountry.country_code;
-    //     country.numericCode = rawCountry.numeric;
-    //     country.continent = this.emptyStringToNull(rawCountry.continent_id);
-    //     country.hdi = this.emptyStringToNull(rawCountry.hdi_id);
-    //     country.region1 = this.emptyStringToNull(rawCountry.region_1);
-    //     country.region2 = this.emptyStringToNull(rawCountry.region_2);
-    //     return country;
-    //   });
-    // }
-    //
-    // // TODO: type raw base data shape (excel)
-    // private processBaseData(rawBaseData: any[]): BaseData[] {
-    //   return rawBaseData.map((rawData) => {
-    //     const baseData = new BaseData();
-    //     baseData.ecosystem = rawData.ecosystem;
-    //     baseData.activity = rawData.activity;
-    //     baseData.countryCode = rawData.country_code;
-    //     baseData.projectSizeHa = this.emptyStringToNull(rawData.project_size_ha);
-    //     baseData.feasibilityAnalysis = this.emptyStringToNull(
-    //       rawData.feseability_analysis,
-    //     );
-    //     baseData.conservationPlanningAndAdmin = this.emptyStringToNull(
-    //       rawData.conservation_planning_and_admin,
-    //     );
-    //     baseData.ecosystemExtent = this.emptyStringToNull(
-    //       rawData.ecosystem_extent,
-    //     );
-    //     baseData.ecosystemLoss = this.emptyStringToNull(rawData.ecosystem_loss);
-    //     baseData.restorableLand = this.emptyStringToNull(rawData.restorable_land);
-    //
-    //     return baseData;
-    //   });
-    // }
-    //
-    // private emptyStringToNull(value: any): any | null {
-    //   return value || null;
-    // }
-    return '' as any;
+    const countries = this.processCountries(data.Countries);
+
+    return {
+      countries,
+    };
+  }
+  //   const { Countries: rawCountries, master_table: rawBaseData } = data;
+  //
+  //   const countries = this.processCountries(rawCountries);
+  //   const baseData = this.processBaseData(rawBaseData);
+  //
+  //   return {
+  //     countries,
+  //     baseData,
+  //   };
+  // }
+  //
+
+  private processCountries(rawCountries: any[]): Country[] {
+    return rawCountries.map((rawCountry) => {
+      const country = new Country();
+      country.name = rawCountry.country;
+      country.code = rawCountry.country_code;
+      country.numericCode = rawCountry.numeric;
+      country.continent = this.emptyStringToNull(rawCountry.continent_id);
+      country.hdi = this.emptyStringToNull(rawCountry.hdi_id);
+      country.region1 = this.emptyStringToNull(rawCountry.region_1);
+      country.region2 = this.emptyStringToNull(rawCountry.region_2);
+      return country;
+    });
+  }
+  //
+  // // TODO: type raw base data shape (excel)
+  // private processBaseData(rawBaseData: any[]): BaseData[] {
+  //   return rawBaseData.map((rawData) => {
+  //     const baseData = new BaseData();
+  //     baseData.ecosystem = rawData.ecosystem;
+  //     baseData.activity = rawData.activity;
+  //     baseData.countryCode = rawData.country_code;
+  //     baseData.projectSizeHa = this.emptyStringToNull(rawData.project_size_ha);
+  //     baseData.feasibilityAnalysis = this.emptyStringToNull(
+  //       rawData.feseability_analysis,
+  //     );
+  //     baseData.conservationPlanningAndAdmin = this.emptyStringToNull(
+  //       rawData.conservation_planning_and_admin,
+  //     );
+  //     baseData.ecosystemExtent = this.emptyStringToNull(
+  //       rawData.ecosystem_extent,
+  //     );
+  //     baseData.ecosystemLoss = this.emptyStringToNull(rawData.ecosystem_loss);
+  //     baseData.restorableLand = this.emptyStringToNull(rawData.restorable_land);
+  //
+  //     return baseData;
+  //   });
+  // }
+  //
+  // private emptyStringToNull(value: any): any | null {
+  //   return value || null;
+  // }
+
+  private emptyStringToNull(value: any): any | null {
+    return value || null;
   }
 }
