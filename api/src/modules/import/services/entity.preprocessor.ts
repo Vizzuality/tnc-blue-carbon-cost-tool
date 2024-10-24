823aa544329ab46f351f4b8e32edc9a409d16dc2
alexeh <alexurola@gmail.com>
2024-10-24 08:08:25 +0200

    first approach to preprocess entities

--- api/src/modules/import/services/entity.preprocessor.ts
+++ api/src/modules/import/services/entity.preprocessor.ts
@@ -2,77 +2,88 @@ import { Injectable } from '@nestjs/common';
 
 import { BaseData } from '@shared/entities/base-data.entity';
 import { Country } from '@shared/entities/country.entity';
+import { BaseDataJson } from '@api/modules/import/excel-base-data.dto';
+import { ProjectSize } from '@shared/entities/project-size.entity';
+import { FeasibilityAnalysis } from '@shared/entities/feasability-analysis.entity';
+import { ConservationPlanningAndAdmin } from '@shared/entities/conservation-and-planning-admin.entity';
+import { CommunityRepresentation } from '@shared/entities/community-representation.entity';
+import { CarbonRights } from '@shared/entities/establishing-carbon-rights.entity';
+import { FinancingCost } from '@shared/entities/financing-cost.entity';
+import { ValidationCost } from '@shared/entities/validation.entity';
+import { ImplementationLaborCost } from '@shared/entities/implementation-labor.entity';
+import { MonitoringCost } from '@shared/entities/monitoring.entity';
+import { Maintenance } from '@shared/entities/maintenance.entity';
+import { DataCollectionAndFieldCosts } from '@shared/entities/data-collection-and-field-costs.entity';
+import { CommunityBenefitSharingFund } from '@shared/entities/community-benefit-sharing-fund.entity';
+import { CarbonStandardFees } from '@shared/entities/carbon-standard-fees.entity';
+import { CommunityCashFlow } from '@shared/entities/community-cash-flow.entity';
 
 export type ParsedDBEntities = {
-  countries: Country[];
-  //baseData: BaseData[];
+  baseData: BaseData[];
 };
 
 @Injectable()
 export class EntityPreprocessor {
-  toDbEntities(data: {
-    Countries: any[];
-    //master_table: any[];
-  }): ParsedDBEntities {
-    const countries = this.processCountries(data.Countries);
-
+  toDbEntities(raw: { rawBaseData: BaseDataJson[] }): ParsedDBEntities {
+    const parsedBaseData = this.processBaseData(raw.rawBaseData);
     return {
-      countries,
+      baseData: parsedBaseData,
     };
   }
-  //   const { Countries: rawCountries, master_table: rawBaseData } = data;
-  //
-  //   const countries = this.processCountries(rawCountries);
-  //   const baseData = this.processBaseData(rawBaseData);
-  //
-  //   return {
-  //     countries,
-  //     baseData,
-  //   };
-  // }
-  //
 
-  private processCountries(rawCountries: any[]): Country[] {
-    return rawCountries.map((rawCountry) => {
-      const country = new Country();
-      country.name = rawCountry.country;
-      country.code = rawCountry.country_code;
-      country.numericCode = rawCountry.numeric;
-      country.continent = this.emptyStringToNull(rawCountry.continent_id);
-      country.hdi = this.emptyStringToNull(rawCountry.hdi_id);
-      country.region1 = this.emptyStringToNull(rawCountry.region_1);
-      country.region2 = this.emptyStringToNull(rawCountry.region_2);
-      return country;
+  private processBaseData(raw: BaseDataJson[]) {
+    const parsedArray: BaseData[] = [];
+    raw.forEach((row: BaseDataJson) => {
+      const baseData = new BaseData();
+      baseData.country = { code: row.country_code } as Country;
+      baseData.activity = row.activity;
+      baseData.ecosystem = row.ecosystem;
+      baseData.projectSize = { sizeHa: row.project_size_ha } as ProjectSize;
+      baseData.feasibilityAnalysis = {
+        analysisScore: row.feasibility_analysis,
+      } as FeasibilityAnalysis;
+      baseData.conservationPlanningAndAdmin = {
+        planningCost: row.conservation_planning_and_admin,
+      } as ConservationPlanningAndAdmin;
+      baseData.communityRepresentation = {
+        liaisonCost: row.community_representation,
+      } as CommunityRepresentation;
+      baseData.carbonRights = {
+        carbonRightsCost: row.establishing_carbon_rights,
+      } as CarbonRights;
+      baseData.financingCost = {
+        financingCostCapexPercent: row.financing_cost,
+      } as FinancingCost;
+      baseData.validationCost = {
+        validationCost: row.validation,
+      } as ValidationCost;
+      baseData.implementationLaborCost = {
+        implementationLaborCost: row.implementation_labor_planting,
+      } as ImplementationLaborCost;
+      baseData.monitoringCost = {
+        monitoringCost: row.monitoring,
+      } as MonitoringCost;
+      baseData.maintenance = {
+        maintenanceCost: row.maintenance,
+      } as Maintenance;
+      baseData.dataCollectionAndFieldCosts = {
+        fieldCost: row.data_collection_and_field_costs,
+      } as DataCollectionAndFieldCosts;
+      baseData.communityBenefit = {
+        benefitSharingFund: row.community_benefit_sharing_fund,
+      } as CommunityBenefitSharingFund;
+      baseData.carbonStandardFees = {
+        carbonStandardFee: row.carbon_standard_fees,
+      } as CarbonStandardFees;
+      baseData.communityCashFlow = {
+        cashflowType: row.other_community_cash_flow,
+      } as CommunityCashFlow;
+
+      parsedArray.push(baseData);
     });
+
+    return parsedArray;
   }
-  //
-  // // TODO: type raw base data shape (excel)
-  // private processBaseData(rawBaseData: any[]): BaseData[] {
-  //   return rawBaseData.map((rawData) => {
-  //     const baseData = new BaseData();
-  //     baseData.ecosystem = rawData.ecosystem;
-  //     baseData.activity = rawData.activity;
-  //     baseData.countryCode = rawData.country_code;
-  //     baseData.projectSizeHa = this.emptyStringToNull(rawData.project_size_ha);
-  //     baseData.feasibilityAnalysis = this.emptyStringToNull(
-  //       rawData.feseability_analysis,
-  //     );
-  //     baseData.conservationPlanningAndAdmin = this.emptyStringToNull(
-  //       rawData.conservation_planning_and_admin,
-  //     );
-  //     baseData.ecosystemExtent = this.emptyStringToNull(
-  //       rawData.ecosystem_extent,
-  //     );
-  //     baseData.ecosystemLoss = this.emptyStringToNull(rawData.ecosystem_loss);
-  //     baseData.restorableLand = this.emptyStringToNull(rawData.restorable_land);
-  //
-  //     return baseData;
-  //   });
-  // }
-  //
-  // private emptyStringToNull(value: any): any | null {
-  //   return value || null;
-  // }
 
   private emptyStringToNull(value: any): any | null {
     return value || null;
