78c8fa85851d92fdcbdc7b6f8e7359658e6e683a
alexeh <alexurola@gmail.com>
2024-10-28 06:41:00 +0100

    wip - project filters

--- api/src/modules/projects/projects.module.ts
+++ api/src/modules/projects/projects.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from '@shared/entities/projects.entity';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
+import { CountriesModule } from '@api/modules/countries/countries.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project])],
+  imports: [TypeOrmModule.forFeature([Project]), CountriesModule],
   controllers: [ProjectsController],
   providers: [ProjectsService],
 })
--- api/src/modules/projects/projects.service.ts
+++ api/src/modules/projects/projects.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { AppBaseService } from '@api/utils/app-base.service';
 import { Project } from '@shared/entities/projects.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
+import { CountryWithNoGeometry } from '@shared/entities/country.entity';
+import { CountriesService } from '@api/modules/countries/countries.service';
 
 @Injectable()
 export class ProjectsService extends AppBaseService<
@@ -14,7 +16,17 @@ export class ProjectsService extends AppBaseService<
   constructor(
     @InjectRepository(Project)
     private readonly projectRepository: Repository<Project>,
+    private readonly countryService: CountriesService,
   ) {
     super(projectRepository, 'project', 'projects');
+  }
+
+  async getProjectCountries(): Promise<CountryWithNoGeometry[]> {
+    const projects = await this.projectRepository.find();
+    const [countries] = await this.countryService.findAll({
+      filter: { code: In(projects.map((p) => p.countryCode)) },
+      omitFields: ['geometry'],
+    });
+    return countries as CountryWithNoGeometry[];
   }
 }
