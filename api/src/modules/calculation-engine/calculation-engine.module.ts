7b91b7c4c3b690594844f433ec359583eb1c4672
alexeh <alexurola@gmail.com>
2024-11-01 08:58:21 +0100

    add calc engine module with hardcoded base size table and base increase

---
+++ api/src/modules/calculation-engine/calculation-engine.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { CalculationEngineService } from './calculation-engine.service';
+
+@Module({
+  providers: [CalculationEngineService]
+})
+export class CalculationEngineModule {}
