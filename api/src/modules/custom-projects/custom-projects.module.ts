22be40dbee262eb15f60917965fb07e0820738e4
alexeh <alexurola@gmail.com>
2024-11-02 05:44:57 +0100

    basic test for getting available countries for custom projects and default model assumptions

--- api/src/modules/custom-projects/custom-projects.module.ts
+++ api/src/modules/custom-projects/custom-projects.module.ts
@@ -3,9 +3,11 @@ import { CustomProjectsService } from '@api/modules/custom-projects/custom-proje
 import { CountriesModule } from '@api/modules/countries/countries.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomProject } from '@shared/entities/custom-project.entity';
+import { CustomProjectsController } from './custom-projects.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CustomProject]), CountriesModule],
   providers: [CustomProjectsService],
+  controllers: [CustomProjectsController],
 })
 export class CustomProjectsModule {}
