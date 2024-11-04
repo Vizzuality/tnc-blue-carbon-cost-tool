b25c0b529173ef15cd0cb99655dd93642cf0af38
Catalin Oancea <catalin.oancea2008@gmail.com>
2024-11-04 18:29:03 +0200

    Add project filtering by name

--- api/src/modules/projects/projects.service.ts
+++ api/src/modules/projects/projects.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { AppBaseService } from '@api/utils/app-base.service';
 import { Project } from '@shared/entities/projects.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, SelectQueryBuilder } from 'typeorm';
+import { FetchSpecification } from 'nestjs-base-service';
 
 @Injectable()
 export class ProjectsService extends AppBaseService<
@@ -16,5 +17,18 @@ export class ProjectsService extends AppBaseService<
     public readonly projectRepository: Repository<Project>,
   ) {
     super(projectRepository, 'project', 'projects');
+  }
+
+  async extendFindAllQuery(
+    query: SelectQueryBuilder<Project>,
+    fetchSpecification: FetchSpecification,
+  ): Promise<SelectQueryBuilder<Project>> {
+    // Filter by project name
+    if (fetchSpecification?.filter?.projectName) {
+      query = query.andWhere('project_name ILIKE :projectName', {
+        projectName: `%${fetchSpecification.filter.projectName}%`,
+      });
+    }
+    return query;
   }
 }
