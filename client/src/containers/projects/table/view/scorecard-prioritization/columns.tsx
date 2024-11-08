62ed06da1f51d031a6741c1db431849252586714
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2024-11-08 12:17:23 +0100

    overview table: uses validated fields

--- client/src/containers/projects/table/view/scorecard-prioritization/columns.tsx
+++ client/src/containers/projects/table/view/scorecard-prioritization/columns.tsx
@@ -1,60 +1,61 @@
-import { ProjectType } from "@shared/contracts/projects.contract";
-import { Project } from "@shared/entities/projects.entity";
-import { createColumnHelper } from "@tanstack/react-table";
-const columnHelper = createColumnHelper<
-  Partial<Project> & {
-    // ! these types should be part of the Project entity eventually, we are adding them here to silent TS for now
-    //   financialFeasibility: number;
-    //   legalFeasibility: number;
-    //   implementationFeasibility: number;
-    //   socialFeasibility: number;
-    //   securityRating: number;
-    //   availabilityOfExperiencedLabor: number;
-    //   availabilityOfAlternatingFunding: number;
-    //   coastalProtectionBenefit: number;
-    //   biodiversityBenefit: number;
-  }
->();
+// import { ProjectType } from "@shared/contracts/projects.contract";
+// import { createColumnHelper } from "@tanstack/react-table";
+// const columnHelper = createColumnHelper<
+//   Partial<Project> & {
+//     // ! these types should be part of the Project entity eventually, we are adding them here to silent TS for now
+//     //   financialFeasibility: number;
+//     //   legalFeasibility: number;
+//     //   implementationFeasibility: number;
+//     //   socialFeasibility: number;
+//     //   securityRating: number;
+//     //   availabilityOfExperiencedLabor: number;
+//     //   availabilityOfAlternatingFunding: number;
+//     //   coastalProtectionBenefit: number;
+//     //   biodiversityBenefit: number;
+//   }
+// >();
 
-export const TABLE_COLUMNS = [
-  columnHelper.accessor("projectName", {
-    enableSorting: true,
-    header: () => <span>Project Name</span>,
-  }),
-  // columnHelper.accessor("financialFeasibility", {
-  //   enableSorting: true,
-  //   header: () => <span>Financial feasibility</span>,
-  // }),
-  // columnHelper.accessor("legalFeasibility", {
-  //   enableSorting: true,
-  //   header: () => <span>Legal Feasibility</span>,
-  // }),
-  // columnHelper.accessor("implementationFeasibility", {
-  //   enableSorting: true,
-  //   header: () => <span>Implementation feasibility</span>,
-  // }),
-  // columnHelper.accessor("socialFeasibility", {
-  //   enableSorting: true,
-  //   header: () => <span>Social Feasibility</span>,
-  // }),
-  // columnHelper.accessor("securityRating", {
-  //   enableSorting: true,
-  //   header: () => <span>Security Rating</span>,
-  // }),
-  // columnHelper.accessor("availabilityOfExperiencedLabor", {
-  //   enableSorting: true,
-  //   header: () => <span>Availability of experienced labor</span>,
-  // }),
-  // columnHelper.accessor("availabilityOfAlternatingFunding", {
-  //   enableSorting: true,
-  //   header: () => <span>Availability of alternating funding</span>,
-  // }),
-  // columnHelper.accessor("coastalProtectionBenefit", {
-  //   enableSorting: true,
-  //   header: () => <span>Coastal Protection benefit</span>,
-  // }),
-  // columnHelper.accessor("biodiversityBenefit", {
-  //   enableSorting: true,
-  //   header: () => <span>Biodiversity benefit</span>,
-  // }),
-];
+export const TABLE_COLUMNS = [];
+
+// export const TABLE_COLUMNS = [
+//   columnHelper.accessor("projectName", {
+//     enableSorting: true,
+//     header: () => <span>Project Name</span>,
+//   }),
+//   columnHelper.accessor("financialFeasibility", {
+//     enableSorting: true,
+//     header: () => <span>Financial feasibility</span>,
+//   }),
+//   columnHelper.accessor("legalFeasibility", {
+//     enableSorting: true,
+//     header: () => <span>Legal Feasibility</span>,
+//   }),
+//   columnHelper.accessor("implementationFeasibility", {
+//     enableSorting: true,
+//     header: () => <span>Implementation feasibility</span>,
+//   }),
+//   columnHelper.accessor("socialFeasibility", {
+//     enableSorting: true,
+//     header: () => <span>Social Feasibility</span>,
+//   }),
+//   columnHelper.accessor("securityRating", {
+//     enableSorting: true,
+//     header: () => <span>Security Rating</span>,
+//   }),
+//   columnHelper.accessor("availabilityOfExperiencedLabor", {
+//     enableSorting: true,
+//     header: () => <span>Availability of experienced labor</span>,
+//   }),
+//   columnHelper.accessor("availabilityOfAlternatingFunding", {
+//     enableSorting: true,
+//     header: () => <span>Availability of alternating funding</span>,
+//   }),
+//   columnHelper.accessor("coastalProtectionBenefit", {
+//     enableSorting: true,
+//     header: () => <span>Coastal Protection benefit</span>,
+//   }),
+//   columnHelper.accessor("biodiversityBenefit", {
+//     enableSorting: true,
+//     header: () => <span>Biodiversity benefit</span>,
+//   }),
+// ];
