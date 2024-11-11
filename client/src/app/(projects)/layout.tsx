eebc68640a2b428b0c85b271cdc8819f2dec8563
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2024-11-11 13:52:41 +0100

    moves sidebar to root layout

--- client/src/app/(projects)/layout.tsx
+++ client/src/app/(projects)/layout.tsx
@@ -6,21 +6,10 @@ import { MapProvider } from "react-map-gl";
 
 import { LayoutGroup } from "framer-motion";
 
-import MainNav from "@/containers/nav";
-
-import { SidebarProvider } from "@/components/ui/sidebar";
-
 export default function BlueCarbonCostLayout({ children }: PropsWithChildren) {
   return (
-    <SidebarProvider>
-      <MapProvider>
-        <main className="flex h-dvh flex-1 overflow-hidden">
-          <LayoutGroup>
-            <MainNav />
-            {children}
-          </LayoutGroup>
-        </main>
-      </MapProvider>
-    </SidebarProvider>
+    <MapProvider>
+      <LayoutGroup>{children}</LayoutGroup>
+    </MapProvider>
   );
 }
