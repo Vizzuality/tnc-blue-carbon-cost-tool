49faea27a44eb8256592c30c54fba2e954f1eb9b
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2024-11-12 18:50:07 +0100

    adds popup to map

---
+++ client/src/components/map/popup/index.tsx
@@ -0,0 +1,41 @@
+import { PropsWithChildren } from "react";
+import * as React from "react";
+
+import { Popup } from "react-map-gl";
+
+import { useAtom } from "jotai";
+import { XIcon } from "lucide-react";
+
+import { popupAtom } from "@/app/(overview)/store";
+
+export default function MapPopup({ children }: PropsWithChildren) {
+  const [popup, setPopup] = useAtom(popupAtom);
+
+  if (!popup || !popup.features?.length) {
+    return null;
+  }
+
+  const closePopup = () => {
+    setPopup(null);
+  };
+
+  return (
+    <Popup
+      longitude={popup.lngLat.lng}
+      latitude={popup.lngLat.lat}
+      closeOnClick={false}
+      onClose={closePopup}
+      className="bg-transparent text-sm"
+      maxWidth="320"
+    >
+      <div className="flex flex-col gap-2">
+        <>
+          <button type="button" onClick={closePopup} className="self-end">
+            <XIcon className="h-5 w-5 text-foreground hover:text-muted-foreground" />
+          </button>
+          {children}
+        </>
+      </div>
+    </Popup>
+  );
+}
