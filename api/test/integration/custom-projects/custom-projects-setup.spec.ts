277348bc6c1a091d2a59f1094653401ab9c1228f
alexeh <alexurola@gmail.com>
2024-11-15 08:46:44 +0100

    update tests

--- api/test/integration/custom-projects/custom-projects-setup.spec.ts
+++ api/test/integration/custom-projects/custom-projects-setup.spec.ts
@@ -46,22 +46,22 @@ describe('Create Custom Projects - Setup', () => {
       });
 
     expect(response.body.data).toMatchObject({
-      feasibilityAnalysis: '50000',
-      conservationPlanningAndAdmin: '166766.66666666666',
-      dataCollectionAndFieldCost: '26666.666666666668',
-      communityRepresentation: '67633.33333333333',
-      blueCarbonProjectPlanning: '100000',
-      establishingCarbonRights: '46666.666666666664',
-      financingCost: '0.05',
-      validation: '50000',
-      implementationLaborHybrid: 'NaN',
-      monitoring: '8400',
-      maintenance: '0.0833',
-      carbonStandardFees: '0.2',
-      communityBenefitSharingFund: '0.5',
-      baselineReassessment: '40000',
-      mrv: '75000',
-      longTermProjectOperatingCost: '22200',
+      feasibilityAnalysis: 50000,
+      conservationPlanningAndAdmin: 166766.66666666666,
+      dataCollectionAndFieldCost: 26666.666666666668,
+      communityRepresentation: 67633.33333333333,
+      blueCarbonProjectPlanning: 100000,
+      establishingCarbonRights: 46666.666666666664,
+      financingCost: 0.05,
+      validation: 50000,
+      implementationLaborHybrid: null,
+      monitoring: 8400,
+      maintenance: 0.0833,
+      carbonStandardFees: 0.2,
+      communityBenefitSharingFund: 0.5,
+      baselineReassessment: 40000,
+      mrv: 75000,
+      longTermProjectOperatingCost: 22200,
     });
   });
 });
