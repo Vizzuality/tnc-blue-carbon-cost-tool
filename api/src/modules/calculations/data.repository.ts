e7b6b7e80bc4773b32194b6238031e7cd65f7501
alexeh <alexurola@gmail.com>
2024-11-18 08:09:58 +0100

    add otherCommunityCashFlow to cost inputs + validate nested properties in create custom project

--- api/src/modules/calculations/data.repository.ts
+++ api/src/modules/calculations/data.repository.ts
@@ -96,6 +96,7 @@ export class DataRepository extends Repository<BaseDataView> {
         'financingCost',
         'implementationLaborPlanting',
         'implementationLaborHydrology',
+        'otherCommunityCashFlow',
       ],
     });
     if (!costInputs) {
