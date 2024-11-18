11cedecc0d07100504f5d6ee2156df071a6c8bcf
alexeh <alexurola@gmail.com>
2024-11-18 06:58:13 +0100

    first calculation feasibility analysis

--- api/src/modules/custom-projects/input-factory/restoration-project.input.ts
+++ api/src/modules/custom-projects/input-factory/restoration-project.input.ts
@@ -1,1 +1,4 @@
-export class RestorationProjectInput {}
+import { ConservationProjectInput } from '@api/modules/custom-projects/input-factory/conservation-project.input';
+
+// TMP extends ConservationProjectInput, until we know how to implement the RestorationProjectInput
+export class RestorationProjectInput extends ConservationProjectInput {}
