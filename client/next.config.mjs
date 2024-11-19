c744a3538096359504128a2a3574300b747ebe73
atrincas <atrincas@gmail.com>
2024-11-19 08:49:44 +0100

    Ignore typeorm errors during nextjs build

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    const warning = [
+      ...(config.ignoreWarnings || []),
+      {
+        module: /typeorm/,
+        message: /the request of a dependency is an expression/,
+      },
+      {
+        module: /typeorm/,
+        message: /Can't resolve 'react-native-sqlite-storage'/,
+      },
+      {
+        module: /typeorm/,
+        message: /Can't resolve '@sap\/hana-client\/extension\/Stream'/,
+      },
+      {
+        module: /typeorm/,
+        message: /Can't resolve 'mysql'/,
+      },
+      {
+        module: /app-root-path/,
+        message: /the request of a dependency is an expression/,
+      },
+    ];
+    config.ignoreWarnings = warning;
+    return config;
+  },
+};
 
 export default nextConfig;
