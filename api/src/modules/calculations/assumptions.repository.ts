ce64a715a85baeee991de82e4a4a7f8e69077d95
alexeh <alexurola@gmail.com>
2024-11-21 07:55:40 +0100

    get filtered assumptions

--- api/src/modules/calculations/assumptions.repository.ts
+++ api/src/modules/calculations/assumptions.repository.ts
@@ -6,8 +6,37 @@ import { GetOverridableAssumptionsDTO } from '@shared/dtos/custom-projects/get-o
 import { ACTIVITY } from '@shared/entities/activity.enum';
 import { ECOSYSTEM } from '@shared/entities/ecosystem.enum';
 
+export enum ECOSYSTEM_RESTORATION_RATE_NAMES {
+  MANGROVE = 'Mangrove restoration rate',
+  SEAGRASS = 'Seagrass restoration rate',
+  SALT_MARSH = 'Salt marsh restoration rate',
+}
+
+export enum ACTIVITY_PROJECT_LENGTH_NAMES {
+  CONSERVATION = 'Conservation project length',
+  RESTORATION = 'Restoration project length',
+}
+
+export const COMMON_OVERRIDABLE_ASSUMPTION_NAMES = [
+  'Baseline reassessment frequency',
+  'Buffer',
+  'Carbon price increase',
+  'Discount rate',
+  'Verification frequency',
+] as const;
+
 @Injectable()
 export class AssumptionsRepository extends Repository<ModelAssumptions> {
+  map: Record<
+    ACTIVITY & ECOSYSTEM,
+    ECOSYSTEM_RESTORATION_RATE_NAMES & ACTIVITY_PROJECT_LENGTH_NAMES
+  > = {
+    [ACTIVITY.CONSERVATION]: ACTIVITY_PROJECT_LENGTH_NAMES.CONSERVATION,
+    [ACTIVITY.RESTORATION]: ACTIVITY_PROJECT_LENGTH_NAMES.RESTORATION,
+    [ECOSYSTEM.MANGROVE]: ECOSYSTEM_RESTORATION_RATE_NAMES.MANGROVE,
+    [ECOSYSTEM.SEAGRASS]: ECOSYSTEM_RESTORATION_RATE_NAMES.SEAGRASS,
+    [ECOSYSTEM.SALT_MARSH]: ECOSYSTEM_RESTORATION_RATE_NAMES.SALT_MARSH,
+  };
   constructor(
     @InjectRepository(ModelAssumptions)
     private repo: Repository<ModelAssumptions>,
@@ -16,44 +45,27 @@ export class AssumptionsRepository extends Repository<ModelAssumptions> {
   }
 
   async getOverridableModelAssumptions(dto: GetOverridableAssumptionsDTO) {
-    const queryBuilder = this.createQueryBuilder('assumptions')
+    const assumptions = await this.createQueryBuilder('model_assumptions')
       .select(['name', 'unit', 'value'])
       .where({
-        name: In([
-          'Verification frequency',
-          'Baseline reassessment frequency',
-          'Carbon price increase',
-          'Buffer',
-          'Discount rate',
-        ]),
-      });
+        name: In(this.getAssumptionNamesByCountryAndEcosystem(dto)),
+      })
+      .orderBy('name', 'ASC')
+      .getRawMany();
+    if (assumptions.length !== 7) {
+      throw new Error('Not all required overridable assumptions were found');
+    }
+    return assumptions;
+  }
 
-    if (dto.activity === ACTIVITY.CONSERVATION) {
-      queryBuilder.andWhere('name = :activity', {
-        activity: 'Conservation project length',
-      });
-    }
-    if (dto.activity === ACTIVITY.RESTORATION) {
-      queryBuilder.andWhere('name = :activity', {
-        activity: 'Restoration project length',
-      });
-    }
-
-    if (dto.ecosystem === ECOSYSTEM.MANGROVE) {
-      queryBuilder.andWhere('name = :ecosystem', {
-        ecosystem: 'Mangrove restoration rate',
-      });
-    }
-    if (dto.ecosystem === ECOSYSTEM.SEAGRASS) {
-      queryBuilder.andWhere('name = :ecosystem', {
-        ecosystem: 'Seagrass restoration rate',
-      });
-    }
-    if (dto.ecosystem === ECOSYSTEM.SALT_MARSH) {
-      queryBuilder.andWhere('name = :ecosystem', {
-        ecosystem: 'Salt marsh restoration rate',
-      });
-    }
+  private getAssumptionNamesByCountryAndEcosystem(
+    dto: GetOverridableAssumptionsDTO,
+  ): string[] {
+    const { ecosystem, activity } = dto;
+    const assumptions = [...COMMON_OVERRIDABLE_ASSUMPTION_NAMES] as string[];
+    assumptions.push(this.map[ecosystem]);
+    assumptions.push(this.map[activity]);
+    return assumptions;
   }
 
   async getAllModelAssumptions() {
