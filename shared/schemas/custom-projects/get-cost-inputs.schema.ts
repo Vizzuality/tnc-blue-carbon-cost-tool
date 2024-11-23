40843d92e53f5fc4d10ca2016498e8ac3781eaef
alexeh <alexurola@gmail.com>
2024-11-23 05:59:20 +0100

    update get cost inputs schema to include restoration subactivity

--- shared/schemas/custom-projects/get-cost-inputs.schema.ts
+++ shared/schemas/custom-projects/get-cost-inputs.schema.ts
@@ -2,8 +2,34 @@ import { z } from "zod";
 import { ECOSYSTEM } from "@shared/entities/ecosystem.enum";
 import { ACTIVITY } from "@shared/entities/activity.enum";
 
-export const GetDefaultCostInputsSchema = z.object({
-  countryCode: z.string().min(3).max(3),
-  ecosystem: z.nativeEnum(ECOSYSTEM),
-  activity: z.nativeEnum(ACTIVITY),
-});
+export const GetDefaultCostInputsSchema = z
+  .object({
+    countryCode: z.string().min(3).max(3),
+    ecosystem: z.nativeEnum(ECOSYSTEM),
+    activity: z.nativeEnum(ACTIVITY),
+    restorationActivity: z.nativeEnum(ECOSYSTEM).optional(),
+  })
+  .superRefine((data, ctx) => {
+    if (
+      data.activity === ACTIVITY.CONSERVATION &&
+      data.restorationActivity !== undefined
+    ) {
+      ctx.addIssue({
+        path: ["restorationActivitySubtype"],
+        message:
+          "restorationActivitySubtype should not be defined when activity is CONSERVATION",
+        code: z.ZodIssueCode.custom,
+      });
+    }
+    if (
+      data.activity === ACTIVITY.RESTORATION &&
+      data.restorationActivity === undefined
+    ) {
+      ctx.addIssue({
+        path: ["restorationActivitySubtype"],
+        message:
+          "restorationActivitySubtype is required when activity is RESTORATION",
+        code: z.ZodIssueCode.custom,
+      });
+    }
+  });
