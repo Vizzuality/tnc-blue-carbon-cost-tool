af3add70b33296a26b56e00680adcebc29817666
alexeh <alexurola@gmail.com>
2024-11-26 07:22:59 +0100

    first approach to sequestration-rate.calculator.ts

--- api/src/modules/calculations/sequestration-rate.calculator.ts
+++ api/src/modules/calculations/sequestration-rate.calculator.ts
@@ -70,12 +70,10 @@ export class SequestrationRateCalculator {
     return netEmissionReductionsPlan;
   }
 
-  private _calculateConservationEmissions(netEmissionReductionsPlan: {
-    [year: number]: number;
-  }): { [year: number]: number } {
-    // "Calcular reducciones de emisiones para proyectos de conservación."
-    const baselineEmissions: { [year: number]: number } =
-      this.calculateBaselineEmissions();
+  private _calculateConservationEmissions(
+    netEmissionReductionsPlan: CostPlanMap,
+  ): CostPlanMap {
+    const baselineEmissions: CostPlanMap = this.calculateBaselineEmissions();
 
     for (const yearStr in netEmissionReductionsPlan) {
       const year = Number(yearStr);
@@ -151,5 +149,252 @@ export class SequestrationRateCalculator {
         plantingSuccessRate
       );
     }
+  }
+
+  calculateBaselineEmissions(): CostPlanMap {
+    // TODO: This is validated previously, but letting it here until we understand what value should we provide for Restoration,
+    //       as all costs are calculated for both types. Maybe this is an internal method and the value is set in another place.
+    if (this.projectInput.activity !== ACTIVITY.CONSERVATION) {
+      console.error('Baseline emissions cannot be calculated for restoration.');
+    }
+
+    const { emissionFactorAgb, emissionFactorSoc, emissionFactor } =
+      this.projectInput;
+    const { tier1SequestrationRate } = this.projectInput.costAndCarbonInputs;
+
+    const baselineEmissionPlan: { [year: number]: number } = {};
+    for (
+      let year = 1;
+      year <= this.projectInput.assumptions.defaultProjectLength;
+      year++
+    ) {
+      if (year !== 0) {
+        baselineEmissionPlan[year] = 0;
+      }
+    }
+
+    const cumulativeLoss = this.calculateCumulativeLossRate();
+    const cumulativeLossRateIncorporatingSOC =
+      this.calculateCumulativeLossRateIncorporatingSOCReleaseTime();
+    const annualAvoidedLoss = this.calculateAnnualAvoidedLoss();
+
+    for (const yearStr in baselineEmissionPlan) {
+      const year = Number(yearStr);
+      let value: number = 0;
+      if (year <= this.projectInput.assumptions.projectLength) {
+        if (emissionFactorSoc && emissionFactorAgb) {
+          value =
+            emissionFactorAgb * annualAvoidedLoss[year] +
+            cumulativeLossRateIncorporatingSOC[year] * emissionFactorSoc +
+            tier1SequestrationRate * cumulativeLoss[year];
+        } else {
+          value =
+            cumulativeLoss[year] * emissionFactor +
+            tier1SequestrationRate * cumulativeLoss[year];
+        }
+        baselineEmissionPlan[year] = value;
+      } else {
+        baselineEmissionPlan[year] = 0;
+      }
+    }
+
+    return baselineEmissionPlan;
+  }
+
+  calculateAreaRestoredOrConserved(): CostPlanMap {
+    const cumulativeHaRestoredInYear: CostPlanMap = {};
+
+    for (
+      let year = -1;
+      year <= this.projectInput.assumptions.defaultProjectLength;
+      year++
+    ) {
+      if (year !== 0) {
+        cumulativeHaRestoredInYear[year] = 0;
+      }
+    }
+
+    for (const yearStr in cumulativeHaRestoredInYear) {
+      const year = Number(yearStr);
+
+      if (year > this.projectInput.assumptions.projectLength) {
+        cumulativeHaRestoredInYear[year] = 0;
+      } else if (this.projectInput.activity === ACTIVITY.RESTORATION) {
+        if (
+          this.projectInput.assumptions.restorationRate <
+          this.projectInput.projectSizeHa
+        ) {
+          cumulativeHaRestoredInYear[year] =
+            this.projectInput.assumptions.restorationRate;
+        } else {
+          cumulativeHaRestoredInYear[year] = this.projectInput.projectSizeHa;
+        }
+      } else {
+        cumulativeHaRestoredInYear[year] = this.projectInput.projectSizeHa;
+      }
+    }
+
+    return cumulativeHaRestoredInYear;
+  }
+
+  calculateCumulativeLossRate(): CostPlanMap {
+    if (this.projectInput.activity !== ACTIVITY.CONSERVATION) {
+      console.error(
+        'Cumulative loss rate cannot be calculated for restoration.',
+      );
+      throw new Error(
+        'Cumulative loss rate cannot be calculated for restoration.',
+      );
+    }
+
+    const cumulativeLossRate: CostPlanMap = {};
+
+    for (
+      let year = 1;
+      year <= this.projectInput.assumptions.defaultProjectLength;
+      year++
+    ) {
+      cumulativeLossRate[year] = 0;
+    }
+
+    const annualAvoidedLoss: { [year: number]: number } =
+      this.calculateAnnualAvoidedLoss();
+
+    for (const yearStr in cumulativeLossRate) {
+      const year = Number(yearStr);
+
+      if (year <= this.projectInput.assumptions.projectLength) {
+        if (year === 1) {
+          cumulativeLossRate[year] = annualAvoidedLoss[year];
+        } else {
+          cumulativeLossRate[year] =
+            annualAvoidedLoss[year] + cumulativeLossRate[year - 1];
+        }
+      } else {
+        cumulativeLossRate[year] = 0;
+      }
+    }
+
+    return cumulativeLossRate;
+  }
+
+  calculateCumulativeLossRateIncorporatingSOCReleaseTime(): CostPlanMap {
+    if (this.projectInput.activity !== ACTIVITY.CONSERVATION) {
+      throw new Error(
+        'La tasa de pérdida acumulada solo puede calcularse para proyectos de conservación.',
+      );
+    }
+
+    const cumulativeLossRateIncorporatingSOC: CostPlanMap = {};
+
+    for (
+      let year = 1;
+      year <= this.projectInput.assumptions.defaultProjectLength;
+      year++
+    ) {
+      cumulativeLossRateIncorporatingSOC[year] = 0;
+    }
+
+    const cumulativeLoss = this.calculateCumulativeLossRate();
+
+    for (const yearStr in cumulativeLossRateIncorporatingSOC) {
+      const year = Number(yearStr);
+
+      if (year <= this.projectInput.assumptions.projectLength) {
+        if (
+          year > this.projectInput.assumptions.soilOrganicCarbonReleaseLength
+        ) {
+          const offsetYear =
+            year - this.projectInput.assumptions.soilOrganicCarbonReleaseLength;
+          const offsetValue = cumulativeLoss[offsetYear];
+          cumulativeLossRateIncorporatingSOC[year] =
+            cumulativeLoss[year] - offsetValue;
+        } else {
+          cumulativeLossRateIncorporatingSOC[year] = cumulativeLoss[year];
+        }
+      } else {
+        cumulativeLossRateIncorporatingSOC[year] = 0;
+      }
+    }
+
+    return cumulativeLossRateIncorporatingSOC;
+  }
+
+  calculateAnnualAvoidedLoss(): CostPlanMap {
+    if (this.projectInput.activity !== ACTIVITY.CONSERVATION) {
+      throw new Error(
+        'Annual avoided loss can only be calculated for conservation projects.',
+      );
+    }
+
+    const projectedLoss: { [year: number]: number } =
+      this.calculateProjectedLoss();
+
+    const annualAvoidedLoss: { [year: number]: number } = {};
+    for (
+      let year = 1;
+      year <= this.projectInput.assumptions.defaultProjectLength;
+      year++
+    ) {
+      annualAvoidedLoss[year] = 0;
+    }
+
+    for (const yearStr in annualAvoidedLoss) {
+      const year = Number(yearStr);
+
+      if (year <= this.projectInput.assumptions.projectLength) {
+        if (year === 1) {
+          annualAvoidedLoss[year] =
+            (projectedLoss[year] - projectedLoss[-1]) * -1;
+        } else {
+          annualAvoidedLoss[year] =
+            (projectedLoss[year] - projectedLoss[year - 1]) * -1;
+        }
+      } else {
+        annualAvoidedLoss[year] = 0;
+      }
+    }
+
+    return annualAvoidedLoss;
+  }
+
+  calculateProjectedLoss(): { [year: number]: number } {
+    if (this.projectInput.activity !== ACTIVITY.CONSERVATION) {
+      throw new Error(
+        'Projected loss can only be calculated for conservation projects.',
+      );
+    }
+
+    const lossRate = this.projectInput.lossRate;
+    const projectSizeHa = this.projectInput.projectSizeHa;
+
+    const annualProjectedLoss: { [year: number]: number } = {};
+
+    for (
+      let year = -1;
+      year <= this.projectInput.assumptions.defaultProjectLength;
+      year++
+    ) {
+      if (year !== 0) {
+        annualProjectedLoss[year] = 0;
+      }
+    }
+
+    for (const yearStr in annualProjectedLoss) {
+      const year = Number(yearStr);
+
+      if (year <= this.projectInput.assumptions.projectLength) {
+        if (year === -1) {
+          annualProjectedLoss[year] = projectSizeHa;
+        } else {
+          annualProjectedLoss[year] =
+            projectSizeHa * Math.pow(1 + lossRate, year);
+        }
+      } else {
+        annualProjectedLoss[year] = 0;
+      }
+    }
+
+    return annualProjectedLoss;
   }
 }
