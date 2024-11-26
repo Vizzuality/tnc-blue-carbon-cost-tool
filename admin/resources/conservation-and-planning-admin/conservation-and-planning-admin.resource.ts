8ab6722b5a238417188b9338560dc84ad5c7b332
onehanddev <hakimuddin.toptal@lottiefiles.com>
2024-11-26 17:03:54 +0530

    Addressed PR review comments.

--- admin/resources/conservation-and-planning-admin/conservation-and-planning-admin.resource.ts
+++ admin/resources/conservation-and-planning-admin/conservation-and-planning-admin.resource.ts
@@ -1,11 +1,11 @@
 import { ResourceWithOptions } from "adminjs";
 import { ConservationPlanningAndAdmin } from "@shared/entities/cost-inputs/conservation-and-planning-admin.entity.js";
-import { GLOBAL_COMMON_PROPERTIES, COMMON_RESOURCE_LIST_PROPERTIES } from "../common/common.resources.js";
+import { GLOBAL_COMMON_PROPERTIES } from "../common/common.resources.js";
 
 export const ConservationAndPlanningAdminResource: ResourceWithOptions = {
   resource: ConservationPlanningAndAdmin,
   options: {
-    properties: {...COMMON_RESOURCE_LIST_PROPERTIES, ...GLOBAL_COMMON_PROPERTIES},
+    properties: {...GLOBAL_COMMON_PROPERTIES},
     sort: {
       sortBy: "planningCost",
       direction: "desc",
