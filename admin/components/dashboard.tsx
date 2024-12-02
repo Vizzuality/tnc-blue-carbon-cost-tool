4faa534c1cd76c1f1d7b71cd2e6951f25455a959
alexeh <alexurola@gmail.com>
2024-12-02 06:35:11 +0100

    override default dashboard

---
+++ admin/components/dashboard.tsx
@@ -0,0 +1,21 @@
+import { Box, H1, Text } from "@adminjs/design-system";
+
+const Dashboard = () => {
+  return (
+    <Box variant="grey">
+      <Box
+        variant="white"
+        flex
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        p="xxl"
+      >
+        <H1>Welcome to Blue Carbon Cost Admin Panel</H1>
+        <Text>Manage your data effectively and efficiently</Text>
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
