46276d0f08ad06ecb22762c7b8a11ce4ffa211bf
Alejandro Peralta <alepefe@protonmail.com>
2024-12-16 10:46:45 +0100

    feat(api): Search projects with maximum values

--- shared/contracts/projects.contract.ts
+++ shared/contracts/projects.contract.ts
@@ -11,32 +11,36 @@ import { ProjectMap } from "@shared/dtos/projects/projects-map.dto";
 import { generateEntityQuerySchema } from "@shared/schemas/query-param.schema";
 import { BaseEntity } from "typeorm";
 import { ProjectScorecardView } from "@shared/entities/project-scorecard.view";
+import { PaginatedProjectsWithMaximums } from "@shared/dtos/projects/projects.dto";
 
 const contract = initContract();
 
 export type ProjectType = Omit<Project, keyof BaseEntity>;
 export type ProjectScorecardType = Omit<ProjectScorecard, keyof BaseEntity>;
 
-export const otherFilters = z.object({
+export const otherParams = z.object({
   costRange: z.coerce.number().array().optional(),
   abatementPotentialRange: z.coerce.number().array().optional(),
   costRangeSelector: z.nativeEnum(COST_TYPE_SELECTOR).optional(),
   partialProjectName: z.string().optional(),
+  withMaximums: z.coerce.boolean().optional(),
 });
 export const projectsQuerySchema = generateEntityQuerySchema(Project);
 export const projectScorecardQuerySchema =
   generateEntityQuerySchema(ProjectScorecard);
 
-export const getProjectsQuerySchema = projectsQuerySchema.merge(otherFilters);
+export const getProjectsQuerySchema = projectsQuerySchema.merge(otherParams);
 export const getProjectScorecardQuerySchema =
-  projectScorecardQuerySchema.merge(otherFilters);
+  projectScorecardQuerySchema.merge(otherParams);
 
 export const projectsContract = contract.router({
   getProjects: {
     method: "GET",
     path: "/projects",
     responses: {
-      200: contract.type<ApiPaginationResponse<Project>>(),
+      200: contract.type<
+        ApiPaginationResponse<Project> | PaginatedProjectsWithMaximums
+      >(),
     },
     query: getProjectsQuerySchema,
   },
