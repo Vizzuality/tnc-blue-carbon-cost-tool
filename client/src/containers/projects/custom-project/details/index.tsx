560d6373b9860f0df0b05da402dabd1c9e0168ef
atrincas <atrincas@gmail.com>
2025-01-15 13:31:13 +0100

    fix: ouput summary percentage values

--- client/src/containers/projects/custom-project/details/index.tsx
+++ client/src/containers/projects/custom-project/details/index.tsx
@@ -5,8 +5,6 @@ import { CARBON_REVENUES_TO_COVER } from "@shared/entities/custom-project.entity
 import { ECOSYSTEM } from "@shared/entities/ecosystem.enum";
 import { FileEdit } from "lucide-react";
 
-import { toPercentageValue } from "@/lib/format";
-
 import { useFeatureFlags } from "@/hooks/use-feature-flags";
 
 import DetailItem from "@/containers/projects/custom-project/details/detail-item";
@@ -94,7 +92,7 @@ const ProjectDetails: FC<ProjectDetailsProps> = ({ data }) => {
           />
           <DetailItem
             label="Loss rate"
-            value={parseFloat(toPercentageValue(lossRate))}
+            value={lossRate}
             unit="%"
             numberFormatOptions={{
               minimumFractionDigits: 2,
