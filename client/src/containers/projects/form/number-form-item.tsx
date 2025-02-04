f94bdbf35a761afc7d7460374d105a03c0c6b979
atrincas <atrincas@gmail.com>
2025-02-04 12:30:56 +0100

    refactor: implement number-form-field component

---
+++ client/src/containers/projects/form/number-form-item.tsx
@@ -0,0 +1,87 @@
+import { useState, ChangeEvent } from "react";
+
+import { toPercentageValue, toDecimalPercentageValue } from "@/lib/format";
+import { cn } from "@/lib/utils";
+
+import { FormControl, FormItem, FormLabel } from "@/components/ui/form";
+import { Input, InputProps } from "@/components/ui/input";
+
+export interface NumberFormItemProps extends InputProps {
+  label: string;
+  tooltip: {
+    title: string;
+    content: string;
+  };
+  initialValue?: string | number;
+  isPercentage?: boolean;
+  formItemClassName?: string;
+  formControlClassName?: string;
+  onValueChange?: (value: number | null) => void;
+  children?: React.ReactNode;
+}
+
+export default function NumberFormItem({
+  label,
+  tooltip,
+  initialValue,
+  isPercentage,
+  formItemClassName,
+  formControlClassName,
+  onValueChange,
+  children,
+  ...props
+}: NumberFormItemProps) {
+  const [value, setValue] = useState<string>(
+    isPercentage && typeof initialValue === "number"
+      ? toPercentageValue(initialValue)
+      : (initialValue?.toString() ?? ""),
+  );
+  const handleOnChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+
+    if (onValueChange) {
+      if (newValue === "") {
+        onValueChange(null);
+      } else {
+        const parsedValue = isPercentage
+          ? toDecimalPercentageValue(newValue)
+          : Number(newValue);
+        onValueChange(parsedValue);
+      }
+    }
+  };
+
+  return (
+    <FormItem className={formItemClassName}>
+      <div className="flex-1">
+        <FormLabel
+          tooltip={{
+            title: tooltip.title,
+            content: tooltip.content,
+          }}
+        >
+          {label}
+        </FormLabel>
+      </div>
+      <FormControl
+        className={cn(
+          "relative after:absolute after:right-9 after:inline-block after:text-sm after:text-muted-foreground",
+          formControlClassName,
+        )}
+      >
+        <div className="relative flex flex-1 items-center">
+          <Input
+            type="number"
+            className="w-full"
+            min={0}
+            value={value}
+            onChange={handleOnChange}
+            {...props}
+          />
+        </div>
+      </FormControl>
+      {children}
+    </FormItem>
+  );
+}
