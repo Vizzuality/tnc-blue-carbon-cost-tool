b8e7d072100971eb9cf2eb4db0c6e60e4414d31f
alexeh <alexurola@gmail.com>
2025-02-06 06:58:36 +0100

    remove restoration-project-params.dto.ts

--- api/src/modules/custom-projects/dto/restoration-project-params.dto.ts
+++
@@ -1,9 +0,0 @@
-import { RESTORATION_ACTIVITY_SUBTYPE } from '@shared/entities/activity.enum';
-import { SEQUESTRATION_RATE_TIER_TYPES } from '@shared/entities/carbon-inputs/sequestration-rate.entity';
-
-export class RestorationProjectParamsDto {
-  activityType: RESTORATION_ACTIVITY_SUBTYPE;
-  sequestrationRateUsed: SEQUESTRATION_RATE_TIER_TYPES;
-  projectSpecificSequestrationRate?: number;
-  plantingSuccessRate: number;
-}
