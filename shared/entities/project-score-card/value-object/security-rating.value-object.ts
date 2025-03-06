d7cefd2526c4a6c40c9512a16f082eb1d6d04829
Alejandro Peralta <alepefe@protonmail.com>
2025-03-06 21:20:39 +0100

    fix(api): Scorecard rating number/string convertion based on each field

---
+++ shared/entities/project-score-card/value-object/security-rating.value-object.ts
@@ -0,0 +1,31 @@
+import { ProjectScoreCardField } from "@shared/entities/project-score-card/value-object/project-score-card-field.value-object";
+import { PROJECT_SCORE } from "@shared/entities/project-score.enum";
+
+export class SecurityRating extends ProjectScoreCardField {
+  private static NUMBER_TO_STRING_MAP: Record<number, PROJECT_SCORE> = {
+    1: PROJECT_SCORE.HIGH,
+    2: PROJECT_SCORE.MEDIUM,
+    3: PROJECT_SCORE.LOW,
+  };
+  private static STRING_TO_NUMBER_MAP: Record<PROJECT_SCORE, number> = {
+    [PROJECT_SCORE.HIGH]: 1,
+    [PROJECT_SCORE.MEDIUM]: 2,
+    [PROJECT_SCORE.LOW]: 3,
+  };
+
+  public static fromString(value: string): SecurityRating {
+    return new SecurityRating(value as PROJECT_SCORE);
+  }
+
+  public static fromNumber(value: number): SecurityRating {
+    return new SecurityRating(SecurityRating.NUMBER_TO_STRING_MAP[value]);
+  }
+
+  public toProjectScore(): PROJECT_SCORE {
+    return this.value;
+  }
+
+  public toNumber(): number {
+    return SecurityRating.STRING_TO_NUMBER_MAP[this.value] ?? 0;
+  }
+}
