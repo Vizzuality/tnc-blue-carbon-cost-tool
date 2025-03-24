18136d154fa4a3f42b51030ef99672b23f6ebb77
Alejandro Peralta <alepefe@protonmail.com>
2025-03-25 00:49:46 +0100

    feat: Allow multiple backoffice sessions for the same user, admin cookies should last the same as the access tokens

--- backoffice/providers/auth.provider.ts
+++ backoffice/providers/auth.provider.ts
@@ -1,16 +1,16 @@
-import { BaseAuthProvider, LoginHandlerOptions } from "adminjs";
-import type { Response } from "express";
-import { ROLES } from "@shared/entities/users/roles.enum.js";
-import { UserDto, UserWithAccessToken } from "@shared/dtos/users/user.dto.js";
-import { API_URL } from "../index.js";
+import { BaseAuthProvider, LoginHandlerOptions } from 'adminjs';
+import type { Response } from 'express';
+import { ROLES } from '@shared/entities/users/roles.enum.js';
+import { UserDto, UserWithAccessToken } from '@shared/dtos/users/user.dto.js';
+import { API_URL } from '../index.js';
 
 export class AuthProvider extends BaseAuthProvider {
   override async handleLogin(opts: LoginHandlerOptions, context?: any) {
     const { email, password } = opts.data;
     try {
       const response = await fetch(`${API_URL}/authentication/login`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
       });
       if (response.ok) {
@@ -33,12 +33,12 @@ export class AuthProvider extends BaseAuthProvider {
     return user.role === ROLES.ADMIN;
   }
 
-  override async handleLogout({res}: {res: Response}) {
+  override async handleLogout({ res }: { res: Response }) {
     // Remove auth cookies
     res.setHeader('Set-Cookie', [
       `backoffice=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly`,
       `next-auth.session-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly`,
-      `__Secure-next-auth.session-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly`,
+      `__Secure-next-auth.session-token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; Secure`,
     ]);
   }
 }
