5b72a014f44e1dddf5360d37f94735a6b73708b6
atrincas <atrincas@gmail.com>
2025-03-25 13:03:17 +0100

    fix(client): callbackurl after signout + backoffice/signout endpoint

--- client/src/containers/profile/profile-sidebar/index.tsx
+++ client/src/containers/profile/profile-sidebar/index.tsx
@@ -4,6 +4,7 @@ import { useAtomValue } from "jotai";
 import { LogOutIcon } from "lucide-react";
 import { signOut, useSession } from "next-auth/react";
 
+import { REDIRECT_SIGNIN_PATH } from "@/lib/constants";
 import { client } from "@/lib/query-client";
 import { queryKeys } from "@/lib/query-keys";
 import { signOutFromBackoffice } from "@/lib/utils";
@@ -66,7 +67,7 @@ const ProfileSidebar: FC<ProfileSidebarProps> = ({ navItems }) => {
         variant="outline"
         className="w-full font-bold"
         onClick={async () => {
-          await signOut();
+          await signOut({ callbackUrl: REDIRECT_SIGNIN_PATH });
           await signOutFromBackoffice();
         }}
       >
