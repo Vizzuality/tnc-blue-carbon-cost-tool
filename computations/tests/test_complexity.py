e318e3f1522e71e4524263a1e341f109dff129ff
Biel Stela <biel.stela@gmail.com>
2025-04-04 10:58:12 +0200

    Init python module

---
+++ computations/tests/test_complexity.py
@@ -0,0 +1,5 @@
+from computations.core import complexity
+
+
+def test_complexity():
+    assert complexity(1, 2) == 3
---
+++ computations/tests/test_main.py
@@ -0,0 +1,15 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from computations.main import app
+
+
+@pytest.fixture
+def client():
+    return TestClient(app)
+
+
+def test_root(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello from computations"}
