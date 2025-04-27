a927abd25fa21799852a6ea70826698b7dce4ec5
alexeh <alexurola@gmail.com>
2025-04-27 06:29:18 +0200

    Add minimal tests to create projects from BO

--- api/test/integration/projects/create-projects.spec.ts
+++ api/test/integration/projects/create-projects.spec.ts
@@ -1,8 +1,10 @@
 import { projectsContract } from '@shared/contracts/projects.contract';
 import { CreateProjectDto } from '@shared/dtos/projects/create-project.dto';
-import { ACTIVITY } from '@shared/entities/activity.enum';
+import {
+  ACTIVITY,
+  RESTORATION_ACTIVITY_SUBTYPE,
+} from '@shared/entities/activity.enum';
 import { ECOSYSTEM } from '@shared/entities/ecosystem.enum';
-import { ProjectScorecard } from '@shared/entities/project-scorecard.entity';
 import { Project, PROJECT_PRICE_TYPE } from '@shared/entities/projects.entity';
 import { TestManager } from '../../utils/test-manager';
 import { TestUser } from '../../utils/user.auth';
@@ -21,10 +23,6 @@ describe('Create projects', () => {
 
   afterEach(async () => {
     await testManager.getDataSource().getRepository(Project).delete({});
-    await testManager
-      .getDataSource()
-      .getRepository(ProjectScorecard)
-      .delete({});
   });
 
   afterAll(async () => {
@@ -32,11 +30,12 @@ describe('Create projects', () => {
     await testManager.close();
   });
 
+  // Conservation Mangrove, country level abatement potential is always 0 because of national average loss rate is always 0 from the data
   test('Create a conservation project with the minimum required fields', async () => {
     const requestBody: CreateProjectDto = {
       countryCode: 'IND',
       projectName: 'Conservation project',
-      ecosystem: ECOSYSTEM.MANGROVE,
+      ecosystem: ECOSYSTEM.SEAGRASS,
       activity: ACTIVITY.CONSERVATION,
       projectSizeHa: 10000,
       initialCarbonPriceAssumption: 20,
@@ -50,5 +49,155 @@ describe('Create projects', () => {
       .send(requestBody);
 
     expect(res.status).toBe(201);
+    const { id, ...project } = res.body.data;
+    expect(project).toEqual(expectedConservationProjectOutput);
+  });
+
+  // Restoration country level is NaN
+  test('Create a Restoration project with the minimum required fields', async () => {
+    const requestBody: CreateProjectDto = {
+      countryCode: 'IND',
+      projectName: 'Restoration project',
+      ecosystem: ECOSYSTEM.MANGROVE,
+      activity: ACTIVITY.RESTORATION,
+      restorationActivity: RESTORATION_ACTIVITY_SUBTYPE.PLANTING,
+      projectSizeHa: 10000,
+      initialCarbonPriceAssumption: 20,
+      priceType: PROJECT_PRICE_TYPE.OPEN_BREAK_EVEN_PRICE,
+    };
+
+    const res = await testManager
+      .request()
+      .post(projectsContract.createProject.path)
+      .set('Cookie', user.backofficeSessionCookie)
+      .send(requestBody);
+
+    expect(res.status).toBe(201);
+    const { id, ...project } = res.body.data;
+    expect(project).toEqual(expectedRestorationProjectOutput);
   });
 });
+
+const expectedConservationProjectOutput = {
+  projectName: 'Conservation project',
+  countryCode: 'IND',
+  ecosystem: 'Seagrass',
+  activity: 'Conservation',
+  restorationActivity: null,
+  projectSize: 10000,
+  priceType: 'Opex breakeven',
+  initialPriceAssumption: 20,
+  scoreCardRating: 'medium',
+  countryAbatementPotential: 81911.08475289373,
+  abatementPotential: 231318.71246113727,
+  totalCostNPV: 3104197.8022359656,
+  totalCost: 4690771.214017419,
+  capexNPV: '1274697.5648611756',
+  capex: '1357600.0000000014',
+  opexNPV: '1829500.23737479',
+  opex: '3333171.214017418',
+  costPerTCO2eNPV: 16.77446330005342,
+  costPerTCO2e: 16.77446330005342,
+  feasibilityAnalysisNPV: '50000',
+  feasibilityAnalysis: '50000',
+  conservationPlanningNPV: '629559.347974512',
+  conservationPlanning: '667066.666666668',
+  dataCollectionNPV: '76962.52465483244',
+  dataCollection: '80000.0000000001',
+  communityRepresentationNPV: '255321.99021392793',
+  communityRepresentation: '270533.3333333332',
+  blueCarbonProjectPlanningNPV: '88899.63586709148',
+  blueCarbonProjectPlanning: '100000',
+  establishingCarbonRightsNPV: '129504.24821726605',
+  establishingCarbonRights: '140000.00000000012',
+  validationNPV: '44449.81793354574',
+  validation: '50000',
+  implementationLaborNPV: '0',
+  implementationLabor: '0',
+  monitoringNPV: '101486.70532413566',
+  monitoring: '168000',
+  maintenanceNPV: '0',
+  maintenance: '0',
+  monitoringMaintenanceNPV: '101486.70532413566',
+  monitoringMaintenance: '168000',
+  communityBenefitNPV: '1196842.5711581379',
+  communityBenefit: '2264160.220023636',
+  carbonStandardFeesNPV: '19847.819786706998',
+  carbonStandardFees: '37010.99399378197',
+  baselineReassessmentNPV: '75811.8711249392',
+  baselineReassessment: '120000',
+  mrvNPV: '167296.40590994002',
+  mrv: '300000',
+  longTermProjectOperatingNPV: '268214.86407093',
+  longTermProjectOperating: '444000',
+  totalRevenueNPV: '2393685.1423162757',
+  totalRevenue: '4528320.440047272',
+  creditsIssued: '185054.96996890983',
+  projectSizeFilter: 'Medium',
+  totalWeightedCost: null,
+  totalWeightedCostNPV: null,
+  leftoverAfterOpexNPV: '0',
+  leftoverAfterOpex: '0',
+} as unknown as Project;
+
+const expectedRestorationProjectOutput = {
+  projectName: 'Restoration project',
+  countryCode: 'IND',
+  ecosystem: 'Mangrove',
+  activity: 'Restoration',
+  restorationActivity: 'Planting',
+  projectSize: 10000,
+  priceType: 'Opex breakeven',
+  initialPriceAssumption: 20,
+  scoreCardRating: 'medium',
+  countryAbatementPotential: 105368.13621180001,
+  abatementPotential: 249480,
+  totalCostNPV: 6502102.945303573,
+  totalCost: 10766906.983822854,
+  capexNPV: '3499180.6740743406',
+  capex: '4959100.000000002',
+  opexNPV: '3002922.2712292327',
+  opex: '5807806.983822852',
+  costPerTCO2eNPV: 32.57827754380899,
+  costPerTCO2e: 32.57827754380899,
+  feasibilityAnalysisNPV: '50000',
+  feasibilityAnalysis: '50000',
+  conservationPlanningNPV: '629559.347974512',
+  conservationPlanning: '667066.666666668',
+  dataCollectionNPV: '76962.52465483244',
+  dataCollection: '80000.0000000001',
+  communityRepresentationNPV: '255321.99021392793',
+  communityRepresentation: '270533.3333333332',
+  blueCarbonProjectPlanningNPV: '88899.63586709148',
+  blueCarbonProjectPlanning: '100000',
+  establishingCarbonRightsNPV: '129504.24821726605',
+  establishingCarbonRights: '140000.00000000012',
+  validationNPV: '44449.81793354574',
+  validation: '50000',
+  implementationLaborNPV: '2224483.109213165',
+  implementationLabor: '3601500',
+  monitoringNPV: '202973.41064827133',
+  monitoring: '336000',
+  maintenanceNPV: '717948.6494182559',
+  maintenance: '1671456.150000001',
+  monitoringMaintenanceNPV: '202973.41064827133',
+  monitoringMaintenance: '336000',
+  communityBenefitNPV: '1281313.4585320817',
+  communityBenefit: '2452434.0338228587',
+  carbonStandardFeesNPV: '21148.747453885157',
+  carbonStandardFees: '39916.80000000001',
+  baselineReassessmentNPV: '75811.8711249392',
+  baselineReassessment: '120000',
+  mrvNPV: '167296.40590994002',
+  mrv: '300000',
+  longTermProjectOperatingNPV: '536429.72814186',
+  longTermProjectOperating: '888000',
+  totalRevenueNPV: '2562626.9170641634',
+  totalRevenue: '4904868.067645717',
+  creditsIssued: '199584',
+  projectSizeFilter: 'Medium',
+  totalWeightedCost: null,
+  totalWeightedCostNPV: null,
+  leftoverAfterOpexNPV: '0',
+  leftoverAfterOpex: '0',
+} as unknown as Project;
