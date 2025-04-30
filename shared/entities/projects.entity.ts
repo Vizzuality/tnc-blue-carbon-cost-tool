a984409f4c367517f907a149d945521bf3e15dc3
alexeh <alexurola@gmail.com>
2025-04-30 08:39:27 +0200

    add empty string to null transformer to handle empty string parsed values
    use new excel version for tests

--- shared/entities/projects.entity.ts
+++ shared/entities/projects.entity.ts
@@ -11,6 +11,7 @@ import { Country } from "@shared/entities/country.entity";
 import { ECOSYSTEM } from "./ecosystem.enum";
 import { ACTIVITY, RESTORATION_ACTIVITY_SUBTYPE } from "./activity.enum";
 import { PROJECT_SCORE } from "@shared/entities/project-score.enum";
+import {EmptyStringToNull} from "@api/utils/empty-string-to-null";
 
 export enum PROJECT_SIZE_FILTER {
   SMALL = "Small",
@@ -56,6 +57,7 @@ export class Project extends BaseEntity {
     type: "enum",
     enum: RESTORATION_ACTIVITY_SUBTYPE,
     nullable: true,
+    transformer: EmptyStringToNull
   })
   restorationActivity: RESTORATION_ACTIVITY_SUBTYPE;
 
