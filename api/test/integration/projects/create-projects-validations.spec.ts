123949a3fb692a4cc31c6adfb7ec1c29e98a3555
alexeh <alexurola@gmail.com>
2025-05-08 06:58:00 +0200

    create projects correct schema, validations

---
+++ api/test/integration/projects/create-projects-validations.spec.ts
@@ -0,0 +1,272 @@
+import { projectsContract } from '@shared/contracts/projects.contract';
+import { CreateProjectDto } from '@shared/dtos/projects/create-project.dto';
+import {
+  ACTIVITY,
+  RESTORATION_ACTIVITY_SUBTYPE,
+} from '@shared/entities/activity.enum';
+import { ECOSYSTEM } from '@shared/entities/ecosystem.enum';
+import {
+  CARBON_REVENUES_TO_COVER,
+  PROJECT_SPECIFIC_EMISSION,
+} from '@shared/entities/custom-project.entity';
+import { EMISSION_FACTORS_TIER_TYPES } from '@shared/entities/carbon-inputs/emission-factors.entity';
+import { SEQUESTRATION_RATE_TIER_TYPES } from '@shared/entities/carbon-inputs/sequestration-rate.entity';
+import { LOSS_RATE_USED } from '@shared/schemas/custom-projects/create-custom-project.schema';
+import { Project } from '@shared/entities/projects.entity';
+import { TestManager } from '../../utils/test-manager';
+import { TestUser } from '../../utils/user.auth';
+
+describe('Create projects Validations', () => {
+  let testManager: TestManager;
+  let user: TestUser;
+
+  beforeAll(async () => {
+    testManager = await TestManager.createTestManager();
+    user = await testManager.setUpTestUser();
+    await testManager.ingestCountries();
+    await testManager.ingestProjectScoreCards(user.jwtToken);
+    await testManager.ingestExcel(user.jwtToken);
+  });
+
+  afterEach(async () => {
+    await testManager.getDataSource().getRepository(Project).delete({});
+  });
+
+  afterAll(async () => {
+    await testManager.clearDatabase();
+    await testManager.close();
+  });
+
+  describe('Conservation project', () => {
+    test('should fail if lossRateUsed is PROJECT_SPECIFIC and projectSpecificLossRate is missing', async () => {
+      const body: CreateProjectDto = {
+        countryCode: 'USA',
+        projectName: 'Conservation X',
+        ecosystem: ECOSYSTEM.MANGROVE,
+        activity: ACTIVITY.CONSERVATION,
+        projectSizeHa: 100,
+        carbonRevenuesToCover: CARBON_REVENUES_TO_COVER.OPEX,
+        initialCarbonPriceAssumption: 40,
+        parameters: {
+          lossRateUsed: LOSS_RATE_USED.PROJECT_SPECIFIC,
+          emissionFactorUsed: EMISSION_FACTORS_TIER_TYPES.TIER_1,
+        },
+      };
+
+      const res = await testManager
+        .request()
+        .post(projectsContract.createProject.path)
+        .set('Cookie', user.backofficeSessionCookie)
+        .send(body);
+
+      expect(res.status).toBe(400);
+      expect(res.body.errors[0].title).toEqual(
+        'Project Specific Loss Rate is required when loss rate used is Project Specific',
+      );
+    });
+
+    test('should fail if loss rate used is national average, but project specific loss rate is provided', async () => {
+      const body: CreateProjectDto = {
+        countryCode: 'USA',
+        projectName: 'Conservation Y',
+        ecosystem: ECOSYSTEM.MANGROVE,
+        activity: ACTIVITY.CONSERVATION,
+        projectSizeHa: 100,
+        carbonRevenuesToCover: CARBON_REVENUES_TO_COVER.OPEX,
+        initialCarbonPriceAssumption: 40,
+        parameters: {
+          lossRateUsed: LOSS_RATE_USED.NATIONAL_AVERAGE,
+          projectSpecificLossRate: -0.12,
+          emissionFactorUsed: EMISSION_FACTORS_TIER_TYPES.TIER_1,
+        },
+      };
+
+      const res = await testManager
+        .request()
+        .post(projectsContract.createProject.path)
+        .set('Cookie', user.backofficeSessionCookie)
+        .send(body);
+
+      expect(res.status).toBe(400);
+      expect(res.body.errors[0].title).toEqual(
+        'projectSpecificLossRate should not be provided unless lossRateUsed is Project Specific',
+      );
+    });
+
+    test('should fail if emission factor used is tier 2 but no emission factors AGB and SOC are provided', async () => {
+      const body: CreateProjectDto = {
+        countryCode: 'USA',
+        projectName: 'Conservation Y',
+        ecosystem: ECOSYSTEM.MANGROVE,
+        activity: ACTIVITY.CONSERVATION,
+        projectSizeHa: 100,
+        carbonRevenuesToCover: CARBON_REVENUES_TO_COVER.OPEX,
+        initialCarbonPriceAssumption: 40,
+        parameters: {
+          lossRateUsed: LOSS_RATE_USED.NATIONAL_AVERAGE,
+          emissionFactorUsed: EMISSION_FACTORS_TIER_TYPES.TIER_2,
+          projectSpecificEmission:
+            PROJECT_SPECIFIC_EMISSION.TWO_EMISSION_FACTORS,
+        },
+      };
+
+      const res = await testManager
+        .request()
+        .post(projectsContract.createProject.path)
+        .set('Cookie', user.backofficeSessionCookie)
+        .send(body);
+
+      expect(res.status).toBe(400);
+      expect(res.body.errors[0].title).toEqual(
+        'Emission Factor AGB and SOC are required when emissionFactorUsed is Tier 2',
+      );
+    });
+
+    test('should fail if Tier 2 is used and ecosystem is not MANGROVE', async () => {
+      const body: CreateProjectDto = {
+        countryCode: 'USA',
+        projectName: 'Wrong Tier 2 Ecosystem',
+        ecosystem: ECOSYSTEM.SEAGRASS,
+        activity: ACTIVITY.CONSERVATION,
+        projectSizeHa: 50,
+        carbonRevenuesToCover: CARBON_REVENUES_TO_COVER.OPEX,
+        initialCarbonPriceAssumption: 30,
+        parameters: {
+          lossRateUsed: LOSS_RATE_USED.NATIONAL_AVERAGE,
+          emissionFactorUsed: EMISSION_FACTORS_TIER_TYPES.TIER_2,
+          emissionFactorAGB: 10,
+          emissionFactorSOC: 5,
+        },
+      };
+
+      const res = await testManager
+        .request()
+        .post(projectsContract.createProject.path)
+        .set('Cookie', user.backofficeSessionCookie)
+        .send(body);
+
+      expect(res.status).toBe(400);
+      expect(res.body.errors[0].title).toEqual(
+        'There is only Tier 2 emission factor for Mangrove ecosystems',
+      );
+    });
+
+    test('should fail if Tier 3 + ONE emission and missing emission factor', async () => {
+      const body: CreateProjectDto = {
+        countryCode: 'USA',
+        projectName: 'Missing One Emission Factor',
+        ecosystem: ECOSYSTEM.MANGROVE,
+        activity: ACTIVITY.CONSERVATION,
+        projectSizeHa: 100,
+        carbonRevenuesToCover: CARBON_REVENUES_TO_COVER.OPEX,
+        initialCarbonPriceAssumption: 50,
+        parameters: {
+          lossRateUsed: LOSS_RATE_USED.PROJECT_SPECIFIC,
+          projectSpecificLossRate: -0.1,
+          emissionFactorUsed: EMISSION_FACTORS_TIER_TYPES.TIER_3,
+          projectSpecificEmission:
+            PROJECT_SPECIFIC_EMISSION.ONE_EMISSION_FACTOR,
+          // Missing projectSpecificEmissionFactor
+        },
+      };
+
+      const res = await testManager
+        .request()
+        .post(projectsContract.createProject.path)
+        .set('Cookie', user.backofficeSessionCookie)
+        .send(body);
+
+      expect(res.status).toBe(400);
+      expect(res.body.errors[0].title).toEqual(
+        'Project Specific Emission Factor must be provided when emissionFactorUsed is Tier 3 and projectSpecificEmission is One emission factor',
+      );
+    });
+
+    test('should fail if Tier 3 + TWO emissions and missing AGB or SOC', async () => {
+      const body: CreateProjectDto = {
+        countryCode: 'USA',
+        projectName: 'Missing AGB or SOC',
+        ecosystem: ECOSYSTEM.MANGROVE,
+        activity: ACTIVITY.CONSERVATION,
+        projectSizeHa: 100,
+        carbonRevenuesToCover: CARBON_REVENUES_TO_COVER.OPEX,
+        initialCarbonPriceAssumption: 50,
+        parameters: {
+          lossRateUsed: LOSS_RATE_USED.PROJECT_SPECIFIC,
+          projectSpecificLossRate: -0.1,
+          emissionFactorUsed: EMISSION_FACTORS_TIER_TYPES.TIER_3,
+          projectSpecificEmission:
+            PROJECT_SPECIFIC_EMISSION.TWO_EMISSION_FACTORS,
+          emissionFactorAGB: 10,
+          // Missing emissionFactorSOC
+        },
+      };
+
+      const res = await testManager
+        .request()
+        .post(projectsContract.createProject.path)
+        .set('Cookie', user.backofficeSessionCookie)
+        .send(body);
+
+      expect(res.status).toBe(400);
+      expect(res.body.errors[0].title).toEqual(
+        'Emission Factor SOC is required when emissionFactorUsed is Tier 3 and projectSpecificEmission is Two emission factors',
+      );
+    });
+  });
+
+  describe('Restoration project', () => {
+    test('should fail if restorationActivity is not provided', async () => {
+      const body: CreateProjectDto = {
+        countryCode: 'USA',
+        projectName: 'Restoration project',
+        ecosystem: ECOSYSTEM.MANGROVE,
+        activity: ACTIVITY.RESTORATION,
+        projectSizeHa: 100,
+        initialCarbonPriceAssumption: 20,
+        carbonRevenuesToCover: CARBON_REVENUES_TO_COVER.OPEX,
+        parameters: {
+          tierSelector: SEQUESTRATION_RATE_TIER_TYPES.TIER_3,
+        },
+      };
+
+      const res = await testManager
+        .request()
+        .post(projectsContract.createProject.path)
+        .set('Cookie', user.backofficeSessionCookie)
+        .send(body);
+
+      expect(res.status).toBe(400);
+      expect(res.body.errors[0].title).toEqual(
+        'Restoration Activity is required for Restoration Projects',
+      );
+    });
+    test('should fail if Restoration uses TIER_3 and projectSpecificSequestrationRate is missing', async () => {
+      const body: CreateProjectDto = {
+        countryCode: 'USA',
+        projectName: 'Missing Sequestration Rate',
+        ecosystem: ECOSYSTEM.MANGROVE,
+        activity: ACTIVITY.RESTORATION,
+        projectSizeHa: 100,
+        initialCarbonPriceAssumption: 50,
+        carbonRevenuesToCover: CARBON_REVENUES_TO_COVER.OPEX,
+        parameters: {
+          restorationActivity: RESTORATION_ACTIVITY_SUBTYPE.PLANTING,
+          tierSelector: SEQUESTRATION_RATE_TIER_TYPES.TIER_3,
+          // Missing projectSpecificSequestrationRate
+        },
+      };
+
+      const res = await testManager
+        .request()
+        .post(projectsContract.createProject.path)
+        .set('Cookie', user.backofficeSessionCookie)
+        .send(body);
+
+      expect(res.status).toBe(400);
+      expect(res.body.errors[0].title).toEqual(
+        'Project Specific Rate is required for Tier 3 Sequestration rate',
+      );
+    });
+  });
+});
--- shared/schemas/projects/create-project.schema.ts
+++ shared/schemas/projects/create-project.schema.ts
@@ -1,5 +1,5 @@
 import {
-  ACTIVITY,
+  ACTIVITY, RESTORATION_ACTIVITY_SUBTYPE,
 } from "@shared/entities/activity.enum";
 import { ECOSYSTEM } from "@shared/entities/ecosystem.enum";
 import { z } from "zod";
@@ -29,17 +29,73 @@ export const CreateProjectBaseSchema = z.object({
   initialCarbonPriceAssumption: z.number().nonnegative(),
 });
 
+
+const parseNumber = (v: unknown) => Number(v);
+
+export const ConservationProjectSchema = z.object({
+  lossRateUsed: z.nativeEnum(LOSS_RATE_USED),
+  emissionFactorUsed: z.nativeEnum(EMISSION_FACTORS_TIER_TYPES),
+  projectSpecificEmission: z.nativeEnum(PROJECT_SPECIFIC_EMISSION).optional(),
+  projectSpecificLossRate: z.preprocess(
+      (value) =>
+          value === undefined || value === null ? undefined : parseNumber(value),
+      z
+          .number({ message: "Project Specific Loss Rate should be a number" })
+          .negative({ message: "Project Specific Loss Rate must be negative" })
+          .optional(),
+  ),
+  projectSpecificEmissionFactor: z
+      .number({
+        message:
+            "Project Specific Emission Factor must be provided when emissionFactorUsed is Tier 3 and projectSpecificEmission is One emission factor",
+      })
+      .nonnegative()
+      .optional(),
+  emissionFactorAGB: z
+      .number({
+        message:
+            "Emission Factor AGB is required when emissionFactorUsed is Tier 3 and projectSpecificEmission is Two emission factors",
+      })
+      .nonnegative()
+      .optional(),
+  emissionFactorSOC: z
+      .number({
+        message:
+            "Emission Factor SOC is required when emissionFactorUsed is Tier 3 and projectSpecificEmission is Two emission factors",
+      })
+      .nonnegative()
+      .optional(),
+});
+
+export const RestorationProjectSchema = z.object({
+  restorationActivity: z.nativeEnum(RESTORATION_ACTIVITY_SUBTYPE, {message: 'Restoration Activity is required for Restoration Projects'}),
+  tierSelector: z.nativeEnum(SEQUESTRATION_RATE_TIER_TYPES),
+  projectSpecificSequestrationRate: z
+      .number({ message: "Project Specific Rate should be a number" })
+      .positive({ message: "Project Specific Rate must be positive" })
+      .optional(),
+  plantingSuccessRate: z.preprocess(
+      parseNumber,
+      z.number().nonnegative({
+        message: "Planting Success Rate should be a non-negative number",
+      }),
+  ).optional(),
+  restorationYearlyBreakdown: z
+      .array(z.preprocess(parseNumber, z.number()).optional())
+      .optional(),
+});
+
 export const CreateProjectSchema = z
     .discriminatedUnion("activity", [
       z.object({
         ...CreateProjectBaseSchema.shape,
         activity: z.literal(ACTIVITY.CONSERVATION),
-        parameters: ConservationCustomProjectSchema,
+        parameters: ConservationProjectSchema,
       }),
       z.object({
         ...CreateProjectBaseSchema.shape,
         activity: z.literal(ACTIVITY.RESTORATION),
-        parameters: RestorationCustomProjectSchema,
+        parameters: RestorationProjectSchema,
       }),
     ])
     .superRefine((data, ctx) => {
@@ -56,13 +112,13 @@ export const ValidateConservationSchema = (
     ctx: z.RefinementCtx,
 ) => {
   const params = data.parameters as z.infer<
-      typeof ConservationCustomProjectSchema
+      typeof ConservationProjectSchema
   >;
   if (params.lossRateUsed === LOSS_RATE_USED.PROJECT_SPECIFIC) {
     if (!params.projectSpecificLossRate) {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
-        message: "required",
+        message: "Project Specific Loss Rate is required when loss rate used is Project Specific",
         path: ["parameters.projectSpecificLossRate"],
       });
     }
@@ -77,12 +133,20 @@ export const ValidateConservationSchema = (
     }
   }
 
+
   if (params.emissionFactorUsed === EMISSION_FACTORS_TIER_TYPES.TIER_2) {
     if (data.ecosystem !== ECOSYSTEM.MANGROVE) {
       ctx.addIssue({
         code: z.ZodIssueCode.custom,
         message: "There is only Tier 2 emission factor for Mangrove ecosystems",
         path: ["parameters.emissionFactorUsed"],
+      });
+    }
+    if(!params.emissionFactorSOC || !params.emissionFactorAGB){
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Emission Factor AGB and SOC are required when emissionFactorUsed is Tier 2",
+        path: ["parameters.emissionFactorAGB", "parameters.emissionFactorSOC"],
       });
     }
   } else if (params.emissionFactorUsed === EMISSION_FACTORS_TIER_TYPES.TIER_3) {
@@ -130,7 +194,7 @@ export const ValidateRestorationSchema = (
     ctx: z.RefinementCtx,
 ) => {
   const params = data.parameters as z.infer<
-      typeof RestorationCustomProjectSchema
+      typeof RestorationProjectSchema
   >;
   if (
       params.tierSelector === SEQUESTRATION_RATE_TIER_TYPES.TIER_3 &&
@@ -138,8 +202,10 @@ export const ValidateRestorationSchema = (
   ) {
     ctx.addIssue({
       code: z.ZodIssueCode.custom,
-      message: "Project Specific Rate is required",
+      message: "Project Specific Rate is required for Tier 3 Sequestration rate",
       path: ["parameters.projectSpecificSequestrationRate"],
     });
   }
 };
+
+
