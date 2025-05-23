9b10160b6309bcac2c48d1803700f0ded74e3308
alexeh <alexurola@gmail.com>
2025-05-23 09:00:30 +0200

    first tests with BE simplest approach

--- shared/schemas/custom-projects/get-restoration-plan.schema.ts
+++ shared/schemas/custom-projects/get-restoration-plan.schema.ts
@@ -3,9 +3,12 @@ import { ECOSYSTEM } from "@shared/entities/ecosystem.enum";
 
 // Schema to get the restoration plan that will be used to create the custom project
 
+// TODO: This is the simplest approach, but we are forcing the FE to always send
+//       the size, length, and rate that will be used. If we agree on this, ecosystem won't be requird
+
 export const GetRestorationPlanSchema = z.object({
-    projectSizeHa: z.number(),
-    ecosystem: z.nativeEnum(ECOSYSTEM),
-    restorationProjectLength: z.number().optional(),
-    restorationRate: z.number().optional(),
+    projectSizeHa: z.preprocess(Number, z.number().positive()),
+    ecosystem: z.nativeEnum(ECOSYSTEM).optional(),
+    restorationProjectLength: z.preprocess(Number, z.number().positive()),
+    restorationRate: z.preprocess(Number, z.number().positive())
 });
