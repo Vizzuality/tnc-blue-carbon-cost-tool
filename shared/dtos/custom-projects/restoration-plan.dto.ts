5339ade7b82777ea9268427a1ddcc3013ab2586c
alexeh <alexurola@gmail.com>
2025-05-23 08:13:58 +0200

    Add restoration plan endpoint contract, get schema and response type

---
+++ shared/dtos/custom-projects/restoration-plan.dto.ts
@@ -0,0 +1,9 @@
+
+// Type for RestorationPlan used for Restoration Custom projects
+
+// TODO: Probably better to use the inferred type from the custom project schema?
+
+export type RestorationPlanDto = {
+    year: number;
+    annualRestoredHectares: number;
+}
