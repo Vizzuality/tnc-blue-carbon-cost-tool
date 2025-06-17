992732ce94cf3e54e66925306c676464dd0c5424
Andrés González <andres.gonzalez@vizzuality.com>
2025-06-17 16:02:46 +0200

    sends projectSpecificLossRate without percentage formatting

--- client/src/containers/projects/form/setup/conservation-project-details/loss-rate.tsx
+++ client/src/containers/projects/form/setup/conservation-project-details/loss-rate.tsx
@@ -91,7 +91,6 @@ export default function LossRate() {
               onValueChange={async (v) =>
                 handleFormChange("parameters.projectSpecificLossRate", v)
               }
-              isPercentage
             />
           )}
         />
