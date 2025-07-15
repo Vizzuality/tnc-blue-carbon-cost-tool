916fccc1f6e502be1c499544d9bb201b5338ac6e
Andrés González <andres.gonzalez@vizzuality.com>
2025-07-15 11:51:17 +0200

    [TBCCT-449] excludes zip file from listing

--- client/src/containers/profile/file-upload/description/index.tsx
+++ client/src/containers/profile/file-upload/description/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 
+import { AVAILABLE_USER_UPLOAD_TEMPLATES } from "@shared/dtos/users/upload-data-files.constants";
 import { UploadDataTemplateDto } from "@shared/dtos/users/upload-data-files.dto";
 import { FileDownIcon } from "lucide-react";
 import { useSession } from "next-auth/react";
@@ -10,6 +11,8 @@ import { getAuthHeader } from "@/lib/utils";
 
 import { Button } from "@/components/ui/button";
 
+const EXCLUSION_FILE_NAMES = [AVAILABLE_USER_UPLOAD_TEMPLATES[0].fileName];
+
 const getDownloadUrl = (file: UploadDataTemplateDto) => {
   // This endpoint is not available in the ts-rest contract
   return `${process.env.NEXT_PUBLIC_API_URL}/users/upload-data/templates/${file.id}`;
@@ -19,6 +22,7 @@ const downloadFile = (f: UploadDataTemplateDto) => {
   link.href = getDownloadUrl(f);
   document.body.appendChild(link);
   link.click();
+  document.body.removeChild(link);
 };
 const openFileUploadWindow = () =>
   document.getElementById("share-information-input")?.click();
@@ -35,7 +39,10 @@ const FileUploadDescription: FC = () => {
     },
     {
       queryKey,
-      select: (data) => data.body.data,
+      select: (data) =>
+        data.body.data.filter(
+          ({ fileName }) => !EXCLUSION_FILE_NAMES.includes(fileName),
+        ),
     },
   );
 
@@ -54,7 +61,7 @@ const FileUploadDescription: FC = () => {
         <Button
           variant="link"
           className="h-auto p-0 text-primary"
-          onClick={() => downloadFile({ id: "0", fileName: "templates.zip" })}
+          onClick={() => downloadFile(AVAILABLE_USER_UPLOAD_TEMPLATES[0])}
         >
           downloadable
         </Button>
